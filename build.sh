e9ad19ea0502a52587e4b63a6c513607156d761c
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2021-07-30 09:01:12 +0200

    adding build commands

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-g++ -std=c++20 src/divsufsort.cpp -ldivsufsort64 -isystem lib/sdsl-lite/include -lfmt -O3 -mpopcnt -mbmi
+#g++ -std=c++20 src/divsufsort.cpp -ldivsufsort64 -isystem lib/sdsl-lite/include -lfmt -O3 -mpopcnt -mbmi -DNDEBUG
+g++ -std=c++20 src/divsufsort.cpp -ldivsufsort64 -isystem lib/sdsl-lite/include -lfmt -O3 -march=native -DNDEBUG
+#g++ -std=c++20 src/divsufsort.cpp -ldivsufsort64 -isystem lib/sdsl-lite/include -lfmt -O0 -ggdb
