42c1c401892cbf38c81d05f311569a34803788dc
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2021-08-05 13:01:14 +0200

    git subrepo clone (merge) git@github.com:xxsds/sdsl-lite.git lib/sdsl-lite
    subrepo:
      subdir:   "lib/sdsl-lite"
      merged:   "163d59b"
    upstream:
      origin:   "git@github.com:xxsds/sdsl-lite.git"
      branch:   "master"
      commit:   "163d59b"
    git-subrepo:
      version:  "0.4.3"
      origin:   "???"
      commit:   "???"

---
+++ lib/sdsl-lite/examples/c++11-initializer-list.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+
+#include <sdsl/suffix_trees.hpp>
+
+using namespace sdsl;
+using namespace std;
+
+int main()
+{
+    int_vector<8> b = { 'c', 'd', 'a', 'b', 'd' };
+    int_vector<> a = { 1, 8, 3, 15, 5 };
+
+    for (auto c : b) { cout << c; }
+    cout << endl;
+    for (auto x : a) { cout << x << ","; }
+    cout << endl;
+    sort(a.begin(), a.end());
+    for (auto x : a) { cout << x << ","; }
+}
---
+++ lib/sdsl-lite/tutorial/expl-20.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <sdsl/suffix_arrays.hpp>
+
+using namespace sdsl;
+using namespace std;
+
+int main()
+{
+    memory_monitor::start();
+    csa_wt<> csa;
+    construct(csa, "english.200MB", 1);
+    memory_monitor::stop();
+    memory_monitor::write_memory_log<HTML_FORMAT>(cout);
+}
