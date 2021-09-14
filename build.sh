6f0071868364caf1a9b0ac5e83f0e0f3c32c0b85
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2021-09-14 17:36:10 +0200

    [PATCH] update build.sh file

--- build.sh
+++ build.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 CXXFLAGS="-std=c++20 -isystem lib/sdsl-lite/include"
-#CXXFLAGS="$CXXFLAGS -O3 -march=native -DNDEBUG"
+CXXFLAGS="$CXXFLAGS -O3 -march=native -DNDEBUG"
 #CXXFLAGS="$CXXFLAGS -O3 -mpopcnt -mbmi -DNDEBUG"
-CXXFLAGS="$CXXFLAGS -O0 -ggdb"
+#CXXFLAGS="$CXXFLAGS -O0 -ggdb"
 
+CXX="ccache g++"
 
 mkdir -p build
-g++ -c $CXXFLAGS src/main.cpp -o build/main.o
-g++ -c $CXXFLAGS src/utils/utils.cpp -o build/utils.o
-g++ -c $CXXFLAGS src/oss/expand.cpp -o build/expand.o
-g++ -c $CXXFLAGS src/oss/isValid.cpp -o build/isValid.o
+$CXX -c $CXXFLAGS src/main.cpp -o build/main.o
+$CXX -c $CXXFLAGS src/utils/utils.cpp -o build/utils.o
+$CXX -c $CXXFLAGS src/oss/expand.cpp -o build/expand.o
+$CXX -c $CXXFLAGS src/oss/isValid.cpp -o build/isValid.o
 
-g++ -c $CXXFLAGS src/oss/generator/h2.cpp -o build/h2.o
-g++ -c $CXXFLAGS src/oss/generator/pigeon.cpp -o build/pigeon.o
-g++ -c $CXXFLAGS src/oss/generator/suffixFilter.cpp -o build/suffixFilter.o
-g++ -c $CXXFLAGS src/oss/generator/zeroOnesZero.cpp -o build/zeroOnesZero.o
+$CXX -c $CXXFLAGS src/oss/generator/h2.cpp -o build/h2.o
+$CXX -c $CXXFLAGS src/oss/generator/pigeon.cpp -o build/pigeon.o
+$CXX -c $CXXFLAGS src/oss/generator/suffixFilter.cpp -o build/suffixFilter.o
+$CXX -c $CXXFLAGS src/oss/generator/zeroOnesZero.cpp -o build/zeroOnesZero.o
 
 
-g++ build/main.o build/utils.o build/expand.o build/isValid.o build/h2.o build/pigeon.o build/suffixFilter.o build/zeroOnesZero.o -ldivsufsort64 -lfmt
+$CXX build/main.o build/utils.o build/expand.o build/isValid.o build/h2.o build/pigeon.o build/suffixFilter.o build/zeroOnesZero.o -ldivsufsort64 -lfmt
