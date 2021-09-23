4a160b30c2a254fc6ff056bff18837ea289b6cda
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2021-09-23 16:45:59 +0200

    [CLEAN] remove Schemes.h, splitting Scheme and SearchTree.h

--- src/search_schemes/Scheme.h
+++ src/search_schemes/Scheme.h
@@ -1,19 +1,9 @@
 #pragma once
 
-#include <tuple>
-#include <vector>
+#include "SearchTree.h"
 
 namespace oss {
 
-struct SearchTree {
-    std::vector<int> pi;
-    std::vector<int> l;
-    std::vector<int> u;
-
-    bool operator==(SearchTree const& _other) const {
-        return std::tie(pi, l, u) == std::tie(_other.pi, _other.l, _other.u);
-    }
-};
 using Scheme = std::vector<SearchTree>;
 
 }
---
+++ src/search_schemes/SearchTree.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <tuple>
+#include <vector>
+
+namespace oss {
+
+struct SearchTree {
+    std::vector<int> pi;
+    std::vector<int> l;
+    std::vector<int> u;
+
+    bool operator==(SearchTree const& _other) const {
+        return std::tie(pi, l, u) == std::tie(_other.pi, _other.l, _other.u);
+    }
+};
+
+}
