faef9c92fa7816509de12be6243c555f5bd3238e
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2021-09-24 13:24:10 +0200

    [FIX] add gen::bestKnown tests

--- src/test_search_schemes/checkGenerators.cpp
+++ src/test_search_schemes/checkGenerators.cpp
@@ -1,5 +1,6 @@
 #include <search_schemes/isValid.h>
 #include <search_schemes/generator/backtracking.h>
+#include <search_schemes/generator/bestKnown.h>
 #include <catch2/catch.hpp>
 
 namespace ss = search_schemes;
@@ -8,13 +9,25 @@ namespace gen = ss::generator;
 TEST_CASE("check search scheme generator backtracking", "[isValid][backtracking]") {
     for (size_t N{1}; N < 10; ++N) { // Number of pieces
         INFO("N " << N);
-        for (size_t minK{0}; minK < 3; ++minK) {
+        for (size_t minK{0}; minK < 6; ++minK) {
             INFO("minK " << minK);
-            for (size_t maxK{minK}; maxK < 4; ++maxK) {
+            for (size_t maxK{minK}; maxK < 6; ++maxK) {
                 INFO("maxK " << maxK);
                 CHECK(ss::isValid(gen::backtracking(N, minK, maxK)));
             }
         }
     }
+}
 
+TEST_CASE("check search scheme generator bestKnown", "[isValid][bestKnown]") {
+    for (size_t N{1}; N < 10; ++N) { // Number of pieces
+        INFO("N " << N);
+        for (size_t minK{0}; minK < 6; ++minK) {
+            INFO("minK " << minK);
+            for (size_t maxK{minK}; maxK < 6; ++maxK) {
+                INFO("maxK " << maxK);
+                CHECK(ss::isValid(gen::bestKnown(N, minK, maxK)));
+            }
+        }
+    }
 }
