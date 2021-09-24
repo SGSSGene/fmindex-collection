3f2785b4575742789efbd64eb57cc703f25f54df
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2021-09-24 13:51:06 +0200

    [CLEANUP] remove unused gen::pigeon functions

--- src/search_schemes/generator/pigeon.h
+++ src/search_schemes/generator/pigeon.h
@@ -4,7 +4,6 @@
 
 namespace search_schemes::generator {
 
-auto pigeon(int minK, int K, int sigma) -> Scheme;
 auto pigeon_trivial(int minK, int K) -> Scheme;
 auto pigeon_opt(int minK, int K) -> Scheme;
 
