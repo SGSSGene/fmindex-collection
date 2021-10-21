7d0fb9b2d534d15e116174cb9b2e649de1906ef2
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2021-10-21 13:21:26 +0200

    [TEST] add test for prefix rank

--- src/test_fmindex-collection/checkOccTables.cpp
+++ src/test_fmindex-collection/checkOccTables.cpp
@@ -27,7 +27,7 @@ TEMPLATE_TEST_CASE("check if occ table is working", "[OccTable]",
             CHECK(table.symbol(i) == text.at(i));
         }
     }
-    SECTION("test complete table 'H'") {
+    SECTION("test complete table 'H' for rank()") {
         CHECK(table.rank( 0, ' ') == 0);
         CHECK(table.rank( 1, ' ') == 0);
         CHECK(table.rank( 2, ' ') == 0);
@@ -123,6 +123,105 @@ TEMPLATE_TEST_CASE("check if occ table is working", "[OccTable]",
         CHECK(table.rank( 8, 't') ==  9);
         CHECK(table.rank( 9, 't') ==  9);
         CHECK(table.rank(10, 't') == 10);
+    }
+
+    SECTION("test complete table 'H' for prefix_rank()") {
+        CHECK(table.prefix_rank( 0, ' ') == 0);
+        CHECK(table.prefix_rank( 1, ' ') == 0);
+        CHECK(table.prefix_rank( 2, ' ') == 0);
+        CHECK(table.prefix_rank( 3, ' ') == 0);
+        CHECK(table.prefix_rank( 4, ' ') == 0);
+        CHECK(table.prefix_rank( 5, ' ') == 0);
+        CHECK(table.prefix_rank( 6, ' ') == 1);
+        CHECK(table.prefix_rank( 7, ' ') == 1);
+        CHECK(table.prefix_rank( 8, ' ') == 1);
+        CHECK(table.prefix_rank( 9, ' ') == 1);
+        CHECK(table.prefix_rank(10, ' ') == 1);
+
+        CHECK(table.prefix_rank( 0, 'H') == 0);
+        CHECK(table.prefix_rank( 1, 'H') == 1);
+        CHECK(table.prefix_rank( 2, 'H') == 1);
+        CHECK(table.prefix_rank( 3, 'H') == 1);
+        CHECK(table.prefix_rank( 4, 'H') == 1);
+        CHECK(table.prefix_rank( 5, 'H') == 1);
+        CHECK(table.prefix_rank( 6, 'H') == 2);
+        CHECK(table.prefix_rank( 7, 'H') == 2);
+        CHECK(table.prefix_rank( 8, 'H') == 2);
+        CHECK(table.prefix_rank( 9, 'H') == 2);
+        CHECK(table.prefix_rank(10, 'H') == 2);
+
+        CHECK(table.prefix_rank( 0, 'W') == 0);
+        CHECK(table.prefix_rank( 1, 'W') == 1);
+        CHECK(table.prefix_rank( 2, 'W') == 1);
+        CHECK(table.prefix_rank( 3, 'W') == 1);
+        CHECK(table.prefix_rank( 4, 'W') == 1);
+        CHECK(table.prefix_rank( 5, 'W') == 1);
+        CHECK(table.prefix_rank( 6, 'W') == 2);
+        CHECK(table.prefix_rank( 7, 'W') == 3);
+        CHECK(table.prefix_rank( 8, 'W') == 3);
+        CHECK(table.prefix_rank( 9, 'W') == 3);
+        CHECK(table.prefix_rank(10, 'W') == 3);
+
+        CHECK(table.prefix_rank( 0, 'a') == 0);
+        CHECK(table.prefix_rank( 1, 'a') == 1);
+        CHECK(table.prefix_rank( 2, 'a') == 2);
+        CHECK(table.prefix_rank( 3, 'a') == 2);
+        CHECK(table.prefix_rank( 4, 'a') == 2);
+        CHECK(table.prefix_rank( 5, 'a') == 2);
+        CHECK(table.prefix_rank( 6, 'a') == 3);
+        CHECK(table.prefix_rank( 7, 'a') == 4);
+        CHECK(table.prefix_rank( 8, 'a') == 4);
+        CHECK(table.prefix_rank( 9, 'a') == 4);
+        CHECK(table.prefix_rank(10, 'a') == 4);
+
+        CHECK(table.prefix_rank( 0, 'e') == 0);
+        CHECK(table.prefix_rank( 1, 'e') == 1);
+        CHECK(table.prefix_rank( 2, 'e') == 2);
+        CHECK(table.prefix_rank( 3, 'e') == 2);
+        CHECK(table.prefix_rank( 4, 'e') == 2);
+        CHECK(table.prefix_rank( 5, 'e') == 2);
+        CHECK(table.prefix_rank( 6, 'e') == 3);
+        CHECK(table.prefix_rank( 7, 'e') == 4);
+        CHECK(table.prefix_rank( 8, 'e') == 5);
+        CHECK(table.prefix_rank( 9, 'e') == 5);
+        CHECK(table.prefix_rank(10, 'e') == 5);
+
+        CHECK(table.prefix_rank( 0, 'l') == 0);
+        CHECK(table.prefix_rank( 1, 'l') == 1);
+        CHECK(table.prefix_rank( 2, 'l') == 2);
+        CHECK(table.prefix_rank( 3, 'l') == 3);
+        CHECK(table.prefix_rank( 4, 'l') == 4);
+        CHECK(table.prefix_rank( 5, 'l') == 4);
+        CHECK(table.prefix_rank( 6, 'l') == 5);
+        CHECK(table.prefix_rank( 7, 'l') == 6);
+        CHECK(table.prefix_rank( 8, 'l') == 7);
+        CHECK(table.prefix_rank( 9, 'l') == 8);
+        CHECK(table.prefix_rank(10, 'l') == 8);
+
+        CHECK(table.prefix_rank( 0, 'o') == 0);
+        CHECK(table.prefix_rank( 1, 'o') == 1);
+        CHECK(table.prefix_rank( 2, 'o') == 2);
+        CHECK(table.prefix_rank( 3, 'o') == 3);
+        CHECK(table.prefix_rank( 4, 'o') == 4);
+        CHECK(table.prefix_rank( 5, 'o') == 5);
+        CHECK(table.prefix_rank( 6, 'o') == 6);
+        CHECK(table.prefix_rank( 7, 'o') == 7);
+        CHECK(table.prefix_rank( 8, 'o') == 8);
+        CHECK(table.prefix_rank( 9, 'o') == 9);
+        CHECK(table.prefix_rank(10, 'o') == 9);
+
+        CHECK(table.prefix_rank( 0, 't') ==  0);
+        CHECK(table.prefix_rank( 1, 't') ==  1);
+        CHECK(table.prefix_rank( 2, 't') ==  2);
+        CHECK(table.prefix_rank( 3, 't') ==  3);
+        CHECK(table.prefix_rank( 4, 't') ==  4);
+        CHECK(table.prefix_rank( 5, 't') ==  5);
+        CHECK(table.prefix_rank( 6, 't') ==  6);
+        CHECK(table.prefix_rank( 7, 't') ==  7);
+        CHECK(table.prefix_rank( 8, 't') ==  8);
+        CHECK(table.prefix_rank( 9, 't') ==  9);
+        CHECK(table.prefix_rank(10, 't') == 10);
 
     }
+
 }
