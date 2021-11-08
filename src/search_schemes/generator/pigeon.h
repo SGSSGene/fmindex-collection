8880189ad985c684263117908bd6253e2b7050ae
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2021-11-08 09:27:31 +0100

    [patch] convert search_schemes to a header only library

--- src/search_schemes/generator/pigeon.h
+++ src/search_schemes/generator/pigeon.h
@@ -2,9 +2,100 @@
 
 #include "../Scheme.h"
 
+#include <cassert>
+
 namespace search_schemes::generator {
 
-auto pigeon_trivial(size_t minK, size_t K) -> Scheme;
-auto pigeon_opt(size_t minK, size_t K) -> Scheme;
+inline auto pigeon_trivial(size_t minK, size_t K) -> Scheme {
+    auto N = K+1;
+
+    assert(N>0);
+    assert(minK <= K);
+    assert(N>K);
+
+    auto res = Scheme{};
+
+    for (size_t i{0}; i < N; ++i) {
+        auto s = Search{};
+        // set order
+        s.pi.push_back(i);
+        for (size_t j{i}; j > 0; --j) {
+            s.pi.push_back(j-1);
+        }
+        for (size_t j{i+1}; j < N; ++j) {
+            s.pi.push_back(j);
+        }
+
+
+        // set lower bound
+        for (size_t j{0}; j < N; ++j) {
+            s.l.push_back(0);
+        }
+
+        // set upper bound
+        s.u.push_back(0);
+        for (size_t j{1}; j < N; ++j) {
+            s.u.push_back(K);
+        }
+        res.push_back(s);
+    }
+
+    // set minimum value
+    for (auto& s : res) {
+        s.l.back() = std::max(s.l.back(), minK);
+    }
+
+    return res;
+}
+
+inline auto pigeon_opt(size_t minK, size_t K) -> Scheme {
+    auto N = K+1;
+
+    assert(N>0);
+    assert(minK <= K);
+    assert(N>K);
+
+    auto res = Scheme{};
+
+    for (size_t i{0}; i < N; ++i) {
+        auto s = Search{};
+        // set order
+        s.pi.push_back(i);
+        for (size_t j{i}; j > 0; --j) {
+            s.pi.push_back(j-1);
+        }
+        for (size_t j{i+1}; j < N; ++j) {
+            s.pi.push_back(j);
+        }
+
+
+        // set lower bound
+        s.l.push_back(0);
+        for (size_t j{i}; j > 0; --j) {
+            s.l.push_back(i-j+1);
+        }
+        for (size_t j{i+1}; j < N; ++j) {
+            s.l.push_back(i);
+        }
+
+        // set upper bound
+        s.u.push_back(0);
+        for (size_t j{i}; j > 0; --j) {
+            s.u.push_back(std::min(K, K-j+1));
+        }
+        for (size_t j{i+1}; j < N; ++j) {
+            s.u.push_back(K);
+        }
+
+        res.push_back(s);
+    }
+
+    // set minimum value
+    for (auto& s : res) {
+        s.l.back() = std::max(s.l.back(), minK);
+    }
+
+    return res;
+}
 
 }
