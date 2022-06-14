c8fc7030c6ee4f4d82375130bbcc48420c71df9f
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-06-14 16:04:30 +0200

    feat: add SelectCursor functionality

--- src/fmindex-collection/search/Backtracking.h
+++ src/fmindex-collection/search/Backtracking.h
@@ -1,33 +1,9 @@
 #pragma once
 
-#include "../BiFMIndexCursor.h"
-#include "../FMIndexCursor.h"
-#include "../ReverseFMIndexCursor.h"
+#include "SelectCursor.h"
 
 namespace fmindex_collection {
 namespace search_backtracking {
-
-template <typename Index>
-struct SelectIndexCursor;
-
-template <typename OccTable, typename TCSA>
-struct SelectIndexCursor<BiFMIndex<OccTable, TCSA>> {
-    using cursor_t = BiFMIndexCursor<BiFMIndex<OccTable, TCSA>>;
-};
-
-template <typename OccTable>
-struct SelectIndexCursor<FMIndex<OccTable>> {
-    using cursor_t = FMIndexCursor<FMIndex<OccTable>>;
-};
-
-template <typename OccTable, typename TCSA>
-struct SelectIndexCursor<ReverseFMIndex<OccTable, TCSA>> {
-    using cursor_t = ReverseFMIndexCursor<ReverseFMIndex<OccTable, TCSA>>;
-};
-
-
-template <typename Index>
-using select_cursor_t = typename SelectIndexCursor<Index>::cursor_t;
 
 /* Search algorithm with explicit programmed search scheme
  */
