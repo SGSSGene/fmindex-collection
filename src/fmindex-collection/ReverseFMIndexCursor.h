6aeccad590d30b905d680f46037162413941d1a6
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-06-23 10:55:41 +0200

    feat: add query_length() to reverse cursor

--- src/fmindex-collection/ReverseFMIndexCursor.h
+++ src/fmindex-collection/ReverseFMIndexCursor.h
@@ -12,19 +12,21 @@ struct ReverseFMIndexCursor {
     Index const* index;
     size_t lb;
     size_t len;
+    size_t depth{};
 
     ReverseFMIndexCursor()
         : index{nullptr}
     {}
 
     ReverseFMIndexCursor(Index const& index)
-        : ReverseFMIndexCursor{index, 0, index.size()}
+        : ReverseFMIndexCursor{index, 0, index.size(), 0}
     {}
 
-    ReverseFMIndexCursor(Index const& index, size_t lb, size_t len)
+    ReverseFMIndexCursor(Index const& index, size_t lb, size_t len, size_t depth)
         : index{&index}
         , lb{lb}
         , len{len}
+        , depth{depth}
     {}
     ReverseFMIndexCursor(ReverseFMIndexCursor const&) = default;
     auto operator=(ReverseFMIndexCursor const&) -> ReverseFMIndexCursor& = default;
@@ -32,16 +34,16 @@ struct ReverseFMIndexCursor {
     auto extendRight(uint8_t symb) const -> ReverseFMIndexCursor {
         size_t newLb  = index->occ.rank(lb, symb);
         size_t newLen = index->occ.rank(lb+len, symb) - newLb;
-        return {*index, newLb, newLen};
+        return {*index, newLb, newLen, depth+1};
     }
     auto extendRight() const -> std::array<ReverseFMIndexCursor, Sigma> {
         auto [rs1, prs1] = index->occ.all_ranks(lb);
         auto [rs2, prs2] = index->occ.all_ranks(lb+len);
 
         auto cursors = std::array<ReverseFMIndexCursor, Sigma>{};
-        cursors[0] = ReverseFMIndexCursor{*index, rs1[0], rs2[0] - rs1[0]};
+        cursors[0] = ReverseFMIndexCursor{*index, rs1[0], rs2[0] - rs1[0], depth+1};
         for (size_t i{1}; i < Sigma; ++i) {
-            cursors[i] = ReverseFMIndexCursor{*index, rs1[i], rs2[i] - rs1[i]};
+            cursors[i] = ReverseFMIndexCursor{*index, rs1[i], rs2[i] - rs1[i], depth+1};
         }
         return cursors;
     }
@@ -52,6 +54,10 @@ struct ReverseFMIndexCursor {
 
     size_t count() const {
         return len;
+    }
+
+    size_t query_length() const {
+        return depth;
     }
 
 };
