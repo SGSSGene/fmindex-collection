5d13cffa18e21ad1cbf2c661d03724c2324e04ca
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-08-08 09:58:41 +0200

    feat: add expectedNodeCount measurement

--- src/run_search_schemes/main.cpp
+++ src/run_search_schemes/main.cpp
@@ -1,5 +1,9 @@
 #include <fmt/format.h>
 #include <search_schemes/generator/pigeon.h>
+#include <search_schemes/nodeCount.h>
+#include <search_schemes/expectedNodeCount.h>
+#include <search_schemes/expand.h>
+
 int main(int argc, char** argv) {
     if (argc == 1) return 0;
 
@@ -11,5 +15,13 @@ int main(int argc, char** argv) {
             fmt::print("{}\n", fmt::join(s.l, ""));
             fmt::print("{}\n\n", fmt::join(s.u, ""));
         }
+
+        ss = search_schemes::expand(ss, 100);
+        fmt::print("node count ham: {}\n",  search_schemes::nodeCount(ss, 4));
+        fmt::print("node count edit: {}\n", search_schemes::nodeCountEdit(ss, 4));
+
+        fmt::print("node count ham: {}\n",  search_schemes::expectedNodeCount(ss, 4, 3'000'000'000));
+        fmt::print("node count edit: {}\n", search_schemes::expectedNodeCountEdit(ss, 4, 3'000'000'000));
+
     }
 }
---
+++ src/search_schemes/expectedNodeCount.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "Scheme.h"
+
+#include <algorithm>
+#include <cassert>
+#include <numeric>
+
+namespace search_schemes {
+
+/**
+ * \param s search scheme
+ * \param sigma size of the alphabet (without delimiter)
+ * \param N     size of the reference text, ~3'000'000'000 for hg
+ */
+inline long double expectedNodeCount(Search s, size_t sigma, size_t N) {
+    auto n_max = s.pi.size();
+    auto e     = *std::max_element(begin(s.u), end(s.u));
+
+    auto lastArray = std::vector<long double>(e+1, 0);
+    lastArray[0] = 1;
+
+    long double acc = 0;
+
+    auto newArray = std::vector<long double>(e+1, 0);
+    for (size_t n {1}; n <= n_max; ++n) {
+        auto f = N / std::pow(sigma, n);
+        if (f > 1) f = 1.;
+
+        for (size_t i{0}; i < e+1; ++i) {
+            if (s.l[n-1] <= i and i <= s.u[n-1]) {
+                newArray[i] = lastArray[i];
+                if (i > 0) {
+                    newArray[i] += (sigma-1) * lastArray[i-1];
+                }
+                newArray[i] *= f;
+                acc += newArray[i];
+
+            } else {
+                newArray[i] = 0;
+            }
+        }
+        std::swap(newArray, lastArray);
+    }
+    return acc;
+}
+
+inline long double expectedNodeCount(Scheme const& ss, size_t sigma, size_t N) {
+    return std::accumulate(begin(ss), end(ss), static_cast<long double>(0.), [&](long double v, auto const& s) {
+        return v + expectedNodeCount(s, sigma, N);
+    });
+}
+
+inline long double expectedNodeCountEdit(Search s, size_t sigma, size_t N) {
+    auto n_max = s.pi.size();
+    auto e     = *std::max_element(begin(s.u), end(s.u));
+
+    auto lastArray = std::vector<long double>(e+1, 0);
+    lastArray[0] = 1;
+
+    long double acc = 0;
+
+    auto newArray = std::vector<long double>(e+1, 0);
+    for (size_t n {1}; n <= n_max; ++n) {
+        auto f = N / std::pow(sigma, n);
+        if (f > 1) f = 1.;
+
+        for (size_t i{0}; i < e+1; ++i) {
+            if (s.l[n-1] <= i and i <= s.u[n-1]) {
+
+                newArray[i] = lastArray[i];
+                if (i > 0) {
+                    newArray[i] += (sigma-1) * lastArray[i-1] + (sigma) * lastArray[i-1] + lastArray[i-1];
+                }
+                newArray[i] *= f;
+                acc += newArray[i];
+            } else {
+                newArray[i] = 0;
+            }
+        }
+        std::swap(newArray, lastArray);
+    }
+    return acc;
+}
+
+inline long double expectedNodeCountEdit(Scheme const& ss, size_t sigma, size_t N) {
+    return std::accumulate(begin(ss), end(ss), static_cast<long double>(0.), [&](long double v, auto const& s) {
+        return v + expectedNodeCountEdit(s, sigma, N);
+    });
+}
+
+}
