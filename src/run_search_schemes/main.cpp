b43eacf653c9ebf462e274999d348c17c70efceb
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-08-08 11:48:37 +0200

    patch: small print of run_search_schemes

--- src/run_search_schemes/main.cpp
+++ src/run_search_schemes/main.cpp
@@ -1,46 +1,47 @@
 #include <fmt/format.h>
-#include <search_schemes/generator/pigeon.h>
+#include <search_schemes/generator/all.h>
 #include <search_schemes/nodeCount.h>
 #include <search_schemes/expectedNodeCount.h>
 #include <search_schemes/expand.h>
+#include <search_schemes/isComplete.h>
 
 int main(int argc, char** argv) {
-    if (argc == 1) return 0;
+    if (argc != 3) {
+        fmt::print("generators: ");
+        for (auto const& [key, val] : search_schemes::generator::all) {
+            fmt::print("{}, ", key);
+        }
+        fmt::print("\n");
+        return 0;
+    }
 
-    auto K = std::stod(argv[1]);
+    auto K   = std::stod(argv[1]);
+    auto gen = argv[2];
     {
-        auto oss = search_schemes::generator::pigeon_opt(0, K);
-        for (auto s : oss) {
+        auto oss = search_schemes::generator::all.at(gen)(0, K, 0, 0);
+//        auto oss = search_schemes::generator::pigeon_opt(0, K);
+/*        for (auto s : oss) {
             fmt::print("{}\n", fmt::join(s.pi, " "));
             fmt::print("{}\n", fmt::join(s.l, " "));
             fmt::print("{}\n\n", fmt::join(s.u, " "));
-        }
+        }*/
 
         auto ss = search_schemes::expand(oss, 100);
-        fmt::print("node count ham: {}\n",  search_schemes::nodeCount(ss, 4));
-        fmt::print("node count edit: {}\n", search_schemes::nodeCountEdit(ss, 4));
+//        fmt::print("node count ham: {}\n",  search_schemes::nodeCount(ss, 4));
+//        fmt::print("node count edit: {}\n", search_schemes::nodeCountEdit(ss, 4));
 
-        fmt::print("node count ham: {}\n",  search_schemes::expectedNodeCount(ss, 4, 3'000'000'000));
-        fmt::print("node count edit: {}\n", search_schemes::expectedNodeCountEdit(ss, 4, 3'000'000'000));
-
-
-        auto counts = search_schemes::expandCount(oss[0].pi.size(), 100);
-        fmt::print("counts {}\n", fmt::join(counts, " "));
-
-        for (auto s : ss) {
-            fmt::print("{}\n", fmt::join(s.pi, " "));
-            fmt::print("{}\n", fmt::join(s.l, " "));
-            fmt::print("{}\n\n", fmt::join(s.u, " "));
-        }
+        auto nc = [&](auto ss) {
+            return search_schemes::expectedNodeCount(ss, 4, 3'000'000'000);
+        };
+        auto nce = [&](auto ss) {
+            return search_schemes::expectedNodeCountEdit(ss, 4, 3'000'000'000);
+        };
         auto ess = search_schemes::expandDynamic(oss, 100, 4, 3'000'000'000);
-        for (auto s : ess) {
-            fmt::print("{}\n", fmt::join(s.pi, " "));
-            fmt::print("{}\n", fmt::join(s.l, " "));
-            fmt::print("{}\n\n", fmt::join(s.u, " "));
-        }
-        fmt::print("node count ham: {}\n",  search_schemes::expectedNodeCount(ess, 4, 3'000'000'000));
-        fmt::print("node count edit: {}\n", search_schemes::expectedNodeCountEdit(ess, 4, 3'000'000'000));
 
+        auto valid = [&](auto ss) {
+            return isValid(ss) and isComplete(ss, 0, K);
+        };
 
+        fmt::print ("{} ham/edit {}/{} → {}/{} ({}, {})\n", K, nc(ss), nce(ss), nc(ess), nce(ess), valid(ss), valid(ess));
     }
 }
