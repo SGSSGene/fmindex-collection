7c321c073248cdd42c9d8c6da3cb39b4a3179703
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-09-01 11:12:17 +0200

    patch: normal search and aborting search can use the same class

--- src/fmindex-collection/search/SearchNg21.h
+++ src/fmindex-collection/search/SearchNg21.h
@@ -38,129 +38,6 @@ struct Search {
         , delegate  {_delegate}
     {}
 
-    void run() {
-        auto cur       = cursor_t{index};
-        auto blockIter = search.begin();
-
-        search_next<'M', 'M'>(cur, 0, blockIter, 0);
-    }
-
-    template <bool Right>
-    static auto extend(cursor_t const& cur, uint8_t symb) noexcept {
-        if constexpr (Right) {
-            return cur.extendRight(symb);
-        } else {
-            return cur.extendLeft(symb);
-        }
-    }
-    template <bool Right>
-    static auto extend(cursor_t const& cur) noexcept {
-        if constexpr (Right) {
-            return cur.extendRight();
-        } else {
-            return cur.extendLeft();
-        }
-    }
-
-
-    template <char LInfo, char RInfo>
-    void search_next(cursor_t const& cur, size_t e, BlockIter blockIter, size_t lastRank) const {
-        if (cur.count() == 0) {
-            return;
-        }
-
-        if (blockIter == end(search)) {
-            if constexpr ((LInfo == 'M' or LInfo == 'I') and (RInfo == 'M' or RInfo == 'I')) {
-                delegate(cur, e);
-            }
-            return;
-        }
-        if (blockIter->dir == Dir::Right) {
-            cur.prefetchRight();
-            search_next_dir<LInfo, RInfo, true>(cur, e, blockIter, lastRank);
-        } else {
-            cur.prefetchLeft();
-            search_next_dir<LInfo, RInfo, false>(cur, e, blockIter, lastRank);
-        }
-    }
-
-    template <char LInfo, char RInfo, bool Right>
-    void search_next_dir(cursor_t const& cur, size_t e, BlockIter blockIter, size_t lastRank) const {
-        static constexpr char TInfo = Right ? RInfo : LInfo;
-
-        constexpr bool Deletion     = TInfo == 'M' or TInfo == 'D';
-        constexpr bool Insertion    = TInfo == 'M' or TInfo == 'I';
-
-        constexpr char OnMatchL      = Right ? LInfo : 'M';
-        constexpr char OnMatchR      = Right ? 'M'   : RInfo;
-        constexpr char OnSubstituteL = Right ? LInfo : 'S';
-        constexpr char OnSubstituteR = Right ? 'S'   : RInfo;
-        constexpr char OnDeletionL   = Right ? LInfo : 'D';
-        constexpr char OnDeletionR   = Right ? 'D'   : RInfo;
-        constexpr char OnInsertionL  = Right ? LInfo : 'I';
-        constexpr char OnInsertionR  = Right ? 'I'   : RInfo;
-
-        auto symb = blockIter->rank;
-
-        bool matchAllowed    = blockIter->l <= e and e <= blockIter->u
-                               and (TInfo != 'I' or symb != (blockIter-1)->rank)
-                               and (TInfo != 'D' or symb != lastRank);
-        bool mismatchAllowed = blockIter->l <= e+1 and e+1 <= blockIter->u;
-
-        if (mismatchAllowed) {
-            auto cursors = extend<Right>(cur);
-
-            if (matchAllowed) {
-                auto newCur = cursors[symb];
-                search_next<OnMatchL, OnMatchR>(newCur, e, blockIter+1, symb);
-            }
-
-            for (uint8_t i{1}; i < symb; ++i) {
-                auto newCur = cursors[i];
-
-                if constexpr (Deletion) {
-                    search_next<OnDeletionL, OnDeletionR>(newCur, e+1, blockIter, i); // deletion occurred in query
-                }
-                search_next<OnSubstituteL, OnSubstituteR>(newCur, e+1, blockIter+1, i); // as substitution
-            }
-
-            for (uint8_t i(symb+1); i < Sigma; ++i) {
-                auto newCur = cursors[i];
-
-                if constexpr (Deletion) {
-                    search_next<OnDeletionL, OnDeletionR>(newCur, e+1, blockIter, i); // deletion occurred in query
-                }
-                search_next<OnSubstituteL, OnSubstituteR>(newCur, e+1, blockIter+1, i); // as substitution
-            }
-
-
-            if constexpr (Insertion) {
-                search_next<OnInsertionL, OnInsertionR>(cur, e+1, blockIter+1, lastRank); // insertion occurred in query
-            }
-        } else if (matchAllowed) {
-            auto newCur = extend<Right>(cur, symb);
-            search_next<OnMatchL, OnMatchR>(newCur, e, blockIter+1, symb);
-        }
-    }
-};
-
-template <typename index_t, typename search_scheme_t, typename delegate_t>
-struct SearchAbort {
-    constexpr static size_t Sigma = index_t::Sigma;
-
-    using cursor_t = BiFMIndexCursor<index_t>;
-    using BlockIter = typename search_scheme_t::const_iterator;
-
-    index_t const& index;
-    search_scheme_t const& search;
-    delegate_t const& delegate;
-
-    SearchAbort(index_t const& _index, search_scheme_t const& _search,  delegate_t const& _delegate)
-        : index     {_index}
-        , search    {_search}
-        , delegate  {_delegate}
-    {}
-
     bool run() {
         auto cur       = cursor_t{index};
         auto blockIter = search.begin();
@@ -283,18 +160,25 @@ void search_reordered(index_t const& index, query_t&& query, search_scheme_t con
     using cursor_t = BiFMIndexCursor<index_t>;
     using R = std::decay_t<decltype(delegate(std::declval<cursor_t>(), 0))>;
 
+    auto internal_delegate = [&]() {
+        if constexpr (std::same_as<R, bool>) {
+            return delegate;
+        } else {
+            return [=](auto cur, size_t e) {
+                delegate(cur, e);
+                return std::false_type{};
+            };
+        }
+    }();
+
     for (size_t j{0}; j < search_scheme.size(); ++j) {
         auto& search = reordered[j];
         for (size_t k {0}; k < search.size(); ++k) {
             search[k].rank = query[search_scheme[j].pi[k]];
         }
-        if constexpr (std::same_as<R, bool>) {
-            bool f = SearchAbort{index, search, delegate}.run();
-            if (f) {
-                return;
-            }
-        } else {
-            Search{index, search, delegate}.run();
+        bool f = Search{index, search, internal_delegate}.run();
+        if (f) {
+            return;
         }
     }
 }
@@ -334,7 +218,6 @@ void search(index_t const & index, queries_t && queries, search_scheme_t const &
 }
 
 
-struct abort_flag {};
 template <typename index_t, typename queries_t, typename search_scheme_t, typename delegate_t>
 void search_n(index_t const & index, queries_t && queries, search_scheme_t const & search_scheme, size_t n, delegate_t && delegate) {
     if (search_scheme.empty()) return;
