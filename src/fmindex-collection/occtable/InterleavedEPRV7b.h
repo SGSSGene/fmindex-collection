8d65fa796363a7b1ef4213154f149fe975c3faff
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-03-13 11:01:55 +0100

    adding more flexible sized OccTables

--- src/fmindex-collection/occtable/InterleavedEPRV7b.h
+++ src/fmindex-collection/occtable/InterleavedEPRV7b.h
@@ -15,18 +15,21 @@
 
 namespace fmindex_collection {
 namespace occtable {
-namespace eprV5_impl {
+namespace interleavedEPRV7b_impl {
 
-template <uint64_t TSigma>
+template <uint64_t _TSigma>
 struct Bitvector {
 
     // number of full length bitvectors needed `2^bitct ≥ TSigma`
-    static constexpr auto bitct = required_bits(TSigma-1);
+    static constexpr auto bitct = required_bits(_TSigma-1);
     // next full power of 2
-    static constexpr auto bvct  = pow(2, bitct);
+    static constexpr auto TSigma  = pow(2, bitct);
 
+
+    #pragma pack(push, 1)
     struct InBits {
         std::array<uint64_t, bitct> bits{};
+        std::array<uint8_t, TSigma> level0{};
 
         uint64_t rank(uint64_t idx, uint64_t symb) const {
             assert(idx < 64);
@@ -38,7 +41,7 @@ struct Bitvector {
             }(std::make_integer_sequence<uint64_t, bitct>{});
 
             auto bitset = std::bitset<64>(mask) << (64-idx);
-            return bitset.count();
+            return bitset.count() + level0[symb];
         }
 
         uint64_t prefix_rank(uint64_t idx, uint64_t symb) const {
@@ -54,7 +57,11 @@ struct Bitvector {
             }
 
             auto bitset = std::bitset<64>(mask) << (64-idx);
-            return bitset.count();
+            auto ct = bitset.count();
+            for (uint64_t i{0}; i <= symb; ++i) {
+                ct += level0[i];
+            }
+            return ct;
         }
 
         auto all_ranks(uint64_t idx) const -> std::array<uint64_t, TSigma> {
@@ -70,7 +77,7 @@ struct Bitvector {
                 auto mask = [&]<uint64_t ...Is>(std::integer_sequence<uint64_t, Is...>) {
                     return (f(i, std::integer_sequence<uint64_t, Is>{})&...);
                 }(std::make_integer_sequence<uint64_t, bitct>{});
-                rs[i] = (std::bitset<64>(mask) << (64 - idx)).count();
+                rs[i] = (std::bitset<64>(mask) << (64 - idx)).count() + level0[i];
             }
             return rs;
         }
@@ -100,30 +107,35 @@ struct Bitvector {
 
             auto bitset = std::bitset<64>{~mask} << (64-idx);
 
-            return {bitset.count(), symb};
+            return {bitset.count() + level0[symb], symb};
         }
 
         template <typename Archive>
         void serialize(Archive& ar) {
-            ar(bits);
+            //!TODO this can be done for sure in a smarter way
+            auto bits_unpacked   = bits;
+            auto level0_unpacked = level0;
+
+            ar(bits_unpacked, level0_unpacked);
+
+            bits   = bits_unpacked;
+            level0 = level0_unpacked;
+
         }
     };
+    #pragma pack(pop)
+
 
     using blockL0_t = uint8_t;
     using blockL1_t = uint16_t;
-//    using blockL2_t = uint32_t;
     static constexpr uint64_t level0_size = sizeof(blockL0_t) * 8;
     static constexpr uint64_t level1_size = sizeof(blockL1_t) * 8;
-//    static constexpr uint64_t level2_size = sizeof(blockL2_t) * 8;
 
     using BlockL0 = std::array<blockL0_t, TSigma>;
     using BlockL1 = std::array<blockL1_t, TSigma>;
-//    using BlockL2 = std::array<blockL2_t, TSigma>;
 
     std::vector<InBits> bits;
-    std::vector<BlockL0> level0;
     std::vector<BlockL1> level1;
-//    std::vector<BlockL2> level2;
     std::vector<std::array<uint64_t, TSigma>> superBlocks;
 
 
@@ -131,14 +143,11 @@ struct Bitvector {
 
     Bitvector(std::span<uint8_t const> _bwt) {
         auto const length = _bwt.size();
-//        level2.reserve(length/(1ull<<level2_size)+2);
         level1.reserve(length/(1ull<<level1_size)+2);
-        level0.reserve(length/64+2);
         bits.reserve(length/64+2);
 
         std::array<blockL0_t, TSigma> blockL0_acc{0};
         std::array<blockL1_t, TSigma> blockL1_acc{0};
-//        std::array<blockL2_t, TSigma> blockL2_acc{0};
         std::array<uint64_t, TSigma> sblock_acc{0};
 
 
@@ -146,26 +155,17 @@ struct Bitvector {
             if (size % (1ull<<level1_size) == 0) { // new l3 block
                 superBlocks.emplace_back(sblock_acc);
                 level1.emplace_back();
-                level0.emplace_back();
                 bits.emplace_back();
                 blockL0_acc = {};
                 blockL1_acc = {};
-/*            } else if (size % (1ull<<level1_size) == 0) { // new l2 block
-                level2.emplace_back(blockL2_acc);
-                level1.emplace_back();
-                level0.emplace_back();
-                bits.emplace_back();
-                blockL0_acc = {};
-                blockL1_acc = {};*/
             } else if (size % (1ull<<level0_size) == 0) { // new l1 block
                 level1.emplace_back(blockL1_acc);
-                level0.emplace_back();
                 bits.emplace_back();
 
                 blockL0_acc = {};
             } else if (size % 64 == 0) { // new l0 block
-                level0.emplace_back(blockL0_acc);
                 bits.emplace_back();
+                bits.back().level0 = blockL0_acc;
             }
             auto level0Id     = size >>  6;
             auto bitId        = size &  63;
@@ -192,56 +192,31 @@ struct Bitvector {
 
     uint64_t memoryUsage() const {
         return    bits.size() * sizeof(bits.back())
-                + level0.size() * sizeof(level0.back())
                 + level1.size() * sizeof(level1.back())
-//                + level2.size() * sizeof(level2.back())
                 + superBlocks.size() * sizeof(superBlocks.back())
                 + sizeof(C);
     }
 
-    void prefetch(uint64_t idx) const {
-        auto level0Id     = idx >>  6;
-        auto level1Id     = idx >> level0_size;
-//        auto level2Id     = idx >> level1_size;
-        auto superBlockId = idx >> level1_size;
-
-//        __builtin_prefetch(reinterpret_cast<void const*>(&level2[level2Id]), 0, 0);
-        __builtin_prefetch(reinterpret_cast<void const*>(&level1[level1Id]), 0, 0);
-        __builtin_prefetch(reinterpret_cast<void const*>(&level0[level0Id]), 0, 0);
-        __builtin_prefetch(reinterpret_cast<void const*>(&bits[level1Id]), 0, 0);
-        __builtin_prefetch(reinterpret_cast<void const*>(&superBlocks[superBlockId]), 0, 0);
-    }
-
     uint64_t rank(uint64_t idx, uint64_t symb) const {
-        prefetch(idx);
-
         auto level0Id     = idx >>  6;
         auto level1Id     = idx >> level0_size;
-//        auto level2Id     = idx >> level1_size;
         auto superBlockId = idx >> level1_size;
         auto bitId        = idx &  63;
         return    bits[level0Id].rank(bitId, symb)
-                + level0[level0Id][symb]
+//                + level0[level0Id][symb]
                 + level1[level1Id][symb]
-//                + level2[level2Id][symb]
                 + superBlocks[superBlockId][symb]
                 + C[symb];
     }
 
     uint64_t prefix_rank(uint64_t idx, uint64_t symb) const {
-        prefetch(idx);
-
         auto level0Id     = idx >>  6;
         auto level1Id     = idx >> level0_size;
-//        auto level2Id     = idx >> level1_size;
         auto superBlockId = idx >> level1_size;
         auto bitId        = idx &  63;
         uint64_t a={};
         for (uint64_t i{0}; i<= symb; ++i) {
-            a +=   level0[level0Id][i]
-                 + level1[level1Id][i]
-//                 + level2[level2Id][i]
-                 + superBlocks[superBlockId][i];
+            a += level1[level1Id][i] + superBlocks[superBlockId][i];
 
         }
         return bits[level0Id].prefix_rank(bitId, symb) + a;
@@ -249,19 +224,14 @@ struct Bitvector {
 
 
     auto all_ranks(uint64_t idx) const -> std::array<uint64_t, TSigma> {
-        prefetch(idx);
-
         auto level0Id     = idx >>  6;
         auto level1Id     = idx >> level0_size;
-//        auto level2Id     = idx >> level1_size;
         auto superBlockId = idx >> level1_size;
         auto bitId        = idx &  63;
         auto res = std::array<uint64_t, TSigma>{};
         for (uint64_t symb{0}; symb < TSigma; ++symb) {
             res[symb] =   bits[level0Id].rank(bitId, symb)
-                        + level0[level0Id][symb]
                         + level1[level1Id][symb]
-//                        + level2[level2Id][symb]
                         + superBlocks[superBlockId][symb]
                         + C[symb];
         }
@@ -269,28 +239,21 @@ struct Bitvector {
     }
 
     auto all_ranks_and_prefix_ranks(uint64_t idx) const -> std::tuple<std::array<uint64_t, TSigma>, std::array<uint64_t, TSigma>> {
-        prefetch(idx);
-
         auto level0Id     = idx >>  6;
         auto level1Id     = idx >> level0_size;
-//        auto level2Id     = idx >> level1_size;
         auto superBlockId = idx >> level1_size;
         auto bitId        = idx &  63;
 
         std::array<uint64_t, TSigma> prs;
         auto rs = bits[level0Id].all_ranks(bitId);
 
-        rs[0] +=   level0[level0Id][0]
-                 + level1[level1Id][0]
-//                 + level2[level2Id][0]
+        rs[0] +=   level1[level1Id][0]
                  + superBlocks[superBlockId][0]
                  + C[0];
 
         prs[0] = rs[0];
         for (uint64_t symb{1}; symb < TSigma; ++symb) {
-            auto a =   level0[level0Id][symb]
-                     + level1[level1Id][symb]
-//                     + level2[level2Id][symb]
+            auto a =   level1[level1Id][symb]
                      + superBlocks[superBlockId][symb];
 
             prs[symb] = prs[symb-1] + rs[symb] + a;
@@ -306,26 +269,21 @@ struct Bitvector {
     }
 
     uint64_t rank_symbol(uint64_t idx) const {
-        prefetch(idx);
-
         auto level0Id     = idx >>  6;
         auto level1Id     = idx >> level0_size;
-//        auto level2Id     = idx >> level1_size;
         auto superBlockId = idx >> level1_size;
         auto bitId = idx & 63;
 
         auto [rank, symb] = bits[level0Id].rank_symbol(bitId);
         return    rank
-                + level0[level0Id][symb]
                 + level1[level1Id][symb]
-//                + level2[level2Id][symb]
                 + superBlocks[superBlockId][symb]
                 + C[symb];
     }
 
     template <typename Archive>
     void serialize(Archive& ar) {
-        ar(bits, level0, level1, superBlocks, C);
+        ar(bits, /*level0,*/ level1, superBlocks, C);
     }
 };
 
@@ -363,9 +321,7 @@ struct OccTable {
         return bitvector.C.back();
     }
 
-    auto prefetch(uint64_t idx) const {
-        bitvector.prefetch(idx);
-    }
+    auto prefetch(uint64_t) const {}
 
     uint64_t rank(uint64_t idx, uint64_t symb) const {
         return bitvector.rank(idx, symb);
@@ -385,7 +341,13 @@ struct OccTable {
 
     auto all_ranks(uint64_t idx) const -> std::tuple<std::array<uint64_t, Sigma>, std::array<uint64_t, Sigma>> {
         auto [rs, prs] = bitvector.all_ranks_and_prefix_ranks(idx);
-        return {rs, prs};
+        auto _rs = std::array<uint64_t, Sigma>{};
+        auto _prs = std::array<uint64_t, Sigma>{};
+        for (size_t i{0}; i < Sigma; ++i) {
+            _rs[i] = rs[i];
+            _prs[i] = prs[i];
+        }
+        return {_rs, _prs};
     }
 
     template <typename Archive>
@@ -397,16 +359,16 @@ struct OccTable {
 
 }
 
-namespace eprV5 {
+namespace interleavedEPRV7b {
 template <uint64_t TSigma>
-struct OccTable : eprV5_impl::OccTable<TSigma> {
-    using eprV5_impl::OccTable<TSigma>::OccTable;
+struct OccTable : interleavedEPRV7b_impl::OccTable<TSigma> {
+    using interleavedEPRV7b_impl::OccTable<TSigma>::OccTable;
     static auto name() -> std::string {
-        return "EPR V5";
+        return "EPR V7b";
     }
 
     static auto extension() -> std::string {
-        return "eprv5";
+        return "ieprv7b";
     }
 };
 static_assert(checkOccTable<OccTable>);
