2aace9c7904dda7f7b83b6a1c1fe8be4118dc3f5
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-11-09 17:57:03 +0100

    feat: add spdx headers and reuse check

--- src/test_search_schemes/main.cpp
+++ src/test_search_schemes/main.cpp
@@ -1,8 +1,5 @@
-// -----------------------------------------------------------------------------------------------------
-// Copyright (c) 2006-2023, Knut Reinert & Freie Universität Berlin
-// Copyright (c) 2016-2023, Knut Reinert & MPI für molekulare Genetik
-// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
-// shipped with this file.
-// -----------------------------------------------------------------------------------------------------
+// SPDX-FileCopyrightText: 2006-2023, Knut Reinert & Freie Universität Berlin
+// SPDX-FileCopyrightText: 2016-2023, Knut Reinert & MPI für molekulare Genetik
+// SPDX-License-Identifier: CC0-1.0
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
