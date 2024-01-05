36412c902e14867f7b42d6cb9cf72f291594d713
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2024-01-05 16:48:23 +0100

    patch: remove unused occtables

--- src/fmindex-collection/occtable/EPR.h
+++ src/fmindex-collection/occtable/EPR.h
@@ -4,12 +4,13 @@
 #pragma once
 
 #include "../rankvector/rankvector.h"
+#include "../bitvector/bitvector.h"
 #include "GenericOccTable.h"
 
-namespace fmindex_collection {
-namespace occtable {
+namespace fmindex_collection::occtable {
 /*!TODO Still needs a few more occtables translated:
  *  - CompactBitvector
+ *  - CompactBitvector4Blocks
  *  - rlebwt
  *
  *  - compactBitvectorPrefix
@@ -25,86 +26,86 @@ static_assert(checkOccTable<Bitvector>);
 
 /*template <uint64_t TSigma>
 using CompactBitvector = GenericOccTable<rankvector::MultiBitvector<TSigma, rankvector::CompactBitvector>, "compact bitvector", "cbv">;
-static_assert(checkOccTable<CompactBitvector>);*/
+static_assert(checkOccTable<CompactBitvector>);
+
+template <uint64_t TSigma>
+using CompactBitvector4Blocks = GenericOccTable<rankvector::MultiBitvector<TSigma, rankvector::CompactBitvector4Blocks>, "compact bitvector 4 blocks", "cbv4">;
+static_assert(checkOccTable<CompactBitvector4Blocks>);*/
+
 
 template <uint64_t TSigma>
 using Epr_8 = GenericOccTable<rankvector::InterleavedEPR8<TSigma>, "Epr (8bit)", "epr_8">;
+static_assert(checkOccTable<Epr_8>);
 
 template <uint64_t TSigma>
 using Epr_16 = GenericOccTable<rankvector::InterleavedEPR16<TSigma>, "Epr (16bit)", "epr_16">;
+static_assert(checkOccTable<Epr_16>);
 
 template <uint64_t TSigma>
 using Epr_32 = GenericOccTable<rankvector::InterleavedEPR32<TSigma>, "Epr (32bit)", "epr_32">;
+static_assert(checkOccTable<Epr_32>);
 
 template <uint64_t TSigma>
 using Epr_8Aligned = GenericOccTable<rankvector::InterleavedEPR8Aligned<TSigma>, "Epr (8bit, aligned)", "epr_8a">;
+static_assert(checkOccTable<Epr_8Aligned>);
 
 template <uint64_t TSigma>
 using Epr_16Aligned = GenericOccTable<rankvector::InterleavedEPR16Aligned<TSigma>, "Epr (16bit, aligned)", "epr_16a">;
+static_assert(checkOccTable<Epr_16Aligned>);
 
 template <uint64_t TSigma>
 using Epr_32Aligned = GenericOccTable<rankvector::InterleavedEPR32Aligned<TSigma>, "Epr (32bit, aligned)", "epr_32a">;
-
-static_assert(checkOccTable<Epr_8>);
-static_assert(checkOccTable<Epr_16>);
-static_assert(checkOccTable<Epr_32>);
-static_assert(checkOccTable<Epr_8Aligned>);
-static_assert(checkOccTable<Epr_16Aligned>);
 static_assert(checkOccTable<Epr_32Aligned>);
 
 
 template <uint64_t TSigma>
 using EprV2_8 = GenericOccTable<rankvector::InterleavedEPRV2_8<TSigma>, "EprV2 (8bit)", "eprv2_8">;
+static_assert(checkOccTable<EprV2_8>);
 
 template <uint64_t TSigma>
 using EprV2_16 = GenericOccTable<rankvector::InterleavedEPRV2_16<TSigma>, "EprV2 (16bit)", "eprv2_16">;
+static_assert(checkOccTable<EprV2_16>);
 
 template <uint64_t TSigma>
 using EprV2_32 = GenericOccTable<rankvector::InterleavedEPRV2_32<TSigma>, "EprV2 (32bit)", "eprv2_32">;
+static_assert(checkOccTable<EprV2_32>);
 
 template <uint64_t TSigma>
 using EprV2_8Aligned = GenericOccTable<rankvector::InterleavedEPRV2_8Aligned<TSigma>, "EprV2 (8bit, aligned)", "eprv2_8a">;
+static_assert(checkOccTable<EprV2_8Aligned>);
 
 template <uint64_t TSigma>
 using EprV2_16Aligned = GenericOccTable<rankvector::InterleavedEPRV2_16Aligned<TSigma>, "EprV2 (16bit, aligned)", "eprv2_16a">;
+static_assert(checkOccTable<EprV2_16Aligned>);
 
 template <uint64_t TSigma>
 using EprV2_32Aligned = GenericOccTable<rankvector::InterleavedEPRV2_32Aligned<TSigma>, "EprV2 (32bit, aligned)", "eprv2_32a">;
-
-static_assert(checkOccTable<EprV2_8>);
-static_assert(checkOccTable<EprV2_16>);
-static_assert(checkOccTable<EprV2_32>);
-static_assert(checkOccTable<EprV2_8Aligned>);
-static_assert(checkOccTable<EprV2_16Aligned>);
 static_assert(checkOccTable<EprV2_32Aligned>);
 
 
 template <uint64_t TSigma>
 using EprV3_8 = GenericOccTable<rankvector::EPRV3_8<TSigma>, "EPRV3 (8bit)", "eprv3_8">;
+static_assert(checkOccTable<EprV3_8>);
 
 template <uint64_t TSigma>
 using EprV3_16 = GenericOccTable<rankvector::EPRV3_16<TSigma>, "EPRV3 (16bit)", "eprv3_16">;
+static_assert(checkOccTable<EprV3_16>);
 
 template <uint64_t TSigma>
 using EprV3_32 = GenericOccTable<rankvector::EPRV3_32<TSigma>, "EPRV3 (32bit)", "eprv3_32">;
-
-static_assert(checkOccTable<EprV3_8>);
-static_assert(checkOccTable<EprV3_16>);
 static_assert(checkOccTable<EprV3_32>);
+
 
 template <uint64_t TSigma>
 using EprV4 = GenericOccTable<rankvector::EPRV4<TSigma>, "EPRV4", "eprv4">;
-
 static_assert(checkOccTable<EprV4>);
 
 template <uint64_t TSigma>
 using EprV5 = GenericOccTable<rankvector::EPRV5<TSigma>, "EPRV5", "eprv5">;
-
 static_assert(checkOccTable<EprV5>);
 
 template <uint64_t TSigma>
 using EprV6 = GenericOccTable<rankvector::DenseEPRV6<TSigma>, "EPRV6", "eprv6">;
-
 static_assert(checkOccTable<EprV6>);
 
 template <uint64_t TSigma>
@@ -123,5 +124,4 @@ template <uint64_t TSigma>
 using Sdsl_wt_bldc = GenericOccTable<rankvector::Sdsl_wt_bldc<TSigma>, "SDSL wt_bldc", "sdsl_wt_bldc">;
 static_assert(checkOccTable<Sdsl_wt_bldc>);
 
-}
 }
