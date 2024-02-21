5dc3193f72c645b2d42433f91ae0a765174b9f85
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2024-02-21 13:21:21 +0100

    feat: used bit vector of Wavelet trees is configurable

--- src/fmindex-collection/rankvector/Wavelet.h
+++ src/fmindex-collection/rankvector/Wavelet.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "../builtins.h"
+#include "../bitvector/Bitvector.h"
 #include "concepts.h"
 #include "utils.h"
 
@@ -22,72 +23,9 @@ namespace fmindex_collection::rankvector {
  *
  * \param TSigma size of the alphabet
  */
-template <size_t TSigma>
+template <size_t TSigma, BitVector_c Bitvector = ::fmindex_collection::bitvector::Bitvector>
 struct Wavelet {
     static constexpr size_t Sigma = TSigma;
-
-    struct alignas(64) Superblock {
-        uint64_t superBlockEntry{};
-        uint64_t blockEntries{};
-        std::array<uint64_t, 6> bits{};
-
-        uint64_t rank(uint64_t idx) const noexcept {
-            assert(idx < 384);
-
-            auto blockId = idx >> 6;
-            auto block = 0b111111111ull & (blockEntries >> (blockId * 9));
-            auto keep = (idx & 63);
-            auto maskedBits = bits[blockId] << (63-keep);
-            auto ct = std::bitset<64>{maskedBits}.count();
-
-            auto total = superBlockEntry + block + ct;
-            return total;
-        }
-
-        bool value(uint64_t idx) const noexcept {
-            assert(idx < 384);
-
-            auto blockId = idx >> 6;
-            auto bitId = idx & 63;
-            return bits[blockId] & (1ull << bitId);
-        }
-
-        void setBlock(uint64_t blockId, uint64_t value) {
-            blockEntries = blockEntries & ~uint64_t{0b111111111ull << blockId*9};
-            blockEntries = blockEntries | uint64_t{value << blockId*9};
-        }
-
-        template <typename Archive>
-        void serialize(Archive& ar) {
-            ar(superBlockEntry, blockEntries, bits);
-        }
-    };
-
-    struct Bitvector {
-        std::vector<Superblock> superblocks{};
-
-        uint64_t rank(uint64_t idx) const noexcept {
-            auto superblockId = idx / 384;
-            auto bitId        = idx % 384;
-            return superblocks[superblockId].rank(bitId);
-        }
-
-        bool value(uint64_t idx) const noexcept {
-            idx += 1;
-            auto superblockId = idx / 384;
-            auto bitId        = idx % 384;
-            return superblocks[superblockId].value(bitId);
-        }
-
-        uint64_t memoryUsage() const {
-            return superblocks.size() * sizeof(superblocks.back());
-        }
-
-        template <typename Archive>
-        void serialize(Archive& ar) {
-            ar(superblocks);
-        }
-    };
 
     static constexpr auto bits = required_bits(TSigma);
     static constexpr auto bvct = pow(2, bits);
@@ -113,56 +51,11 @@ struct Wavelet {
             }
         };
 
-        std::array<uint64_t, TSigma> symb_count{};
-        for (uint64_t size{0}; size < length; ++size) {
-            auto symb = _symbols[size];
-            symb_count[symb] += 1;
-        }
-
-        std::array<uint64_t, bvct> count{};
-        for (uint64_t i{0}; i < TSigma; ++i) {
-            which_bv(i, [&](uint64_t id, [[maybe_unused]] uint64_t bit) {
-                count[id] += symb_count[i];
-            });
-        }
-
-        for (uint64_t j{0}; j < bvct; ++j) {
-            bitvector[j].superblocks.reserve(count[j]/384+1);
-            bitvector[j].superblocks.emplace_back();
-        }
-
-        std::array<uint64_t, bvct> sblock_acc{0};
-        std::array<uint16_t, bvct> block_acc{0};
-        std::array<uint64_t, bvct> bv_size{};
-        for (auto& c : bv_size) {
-            c = 1;
-        }
-
-        auto add_bv_bit = [&](uint64_t bv_id, uint64_t value) {
-            auto size = bv_size[bv_id];
-            if (size % 384 == 0) { // new super block + new block
-                bitvector[bv_id].superblocks.emplace_back();
-                bitvector[bv_id].superblocks.back().superBlockEntry = sblock_acc[bv_id];
-                block_acc[bv_id] = 0;
-            } else if (size % 64 == 0) { // new block
-                bitvector[bv_id].superblocks.back().setBlock((size % 384) / 64, block_acc[bv_id]);
-            }
-
-            auto blockId      = (size >>  6) % 6;
-            auto bitId        = size &  63;
-
-            auto& bits = bitvector[bv_id].superblocks.back().bits[blockId];
-            bits = bits | (value << bitId);
-
-            block_acc[bv_id]  += value;
-            sblock_acc[bv_id] += value;
-            bv_size[bv_id] += 1;
-        };
 
         for (uint64_t size{1}; size <= length; ++size) {
             auto symb = _symbols[size-1];
             which_bv(symb, [&](uint64_t id, uint64_t bit) {
-                add_bv_bit(id, bit);
+                bitvector[id].push_back(bit);
             });
         }
     }
@@ -257,7 +150,9 @@ struct Wavelet {
     }
 
 };
+template <uint64_t TSigma>
+using Wavelet_Default = Wavelet<TSigma>;
 
-static_assert(checkRankVector<Wavelet>);
+static_assert(checkRankVector<Wavelet_Default>);
 
 }
