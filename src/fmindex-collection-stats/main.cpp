ceb8ab49cf0a52da996330c676376db315398f28
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2024-02-21 14:06:07 +0100

    fix: adding missing license information

--- src/fmindex-collection-stats/main.cpp
+++ src/fmindex-collection-stats/main.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2006-2023, Knut Reinert & Freie Universität Berlin
+// SPDX-FileCopyrightText: 2016-2023, Knut Reinert & MPI für molekulare Genetik
+// SPDX-License-Identifier: CC0-1.0
 #include <fmindex-collection/rankvector/rankvector.h>
 #include <fmindex-collection/bitvector/all.h>
 #include <fmt/format.h>
