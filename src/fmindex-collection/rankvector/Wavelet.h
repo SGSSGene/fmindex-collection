0041da0c0c5377effb1e8f8747bc1a90edb1d534
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2024-08-09 10:30:07 +0200

    feat: add PrunedWavelet for rank support on vectors

--- src/fmindex-collection/rankvector/Wavelet.h
+++ src/fmindex-collection/rankvector/Wavelet.h
@@ -5,6 +5,7 @@
 
 #include "../builtins.h"
 #include "../bitvector/Bitvector.h"
+#include "../bitvector/PrunedBitvector.h"
 #include "concepts.h"
 #include "utils.h"
 
@@ -27,36 +28,45 @@ template <size_t TSigma, BitVector_c Bitvector = ::fmindex_collection::bitvector
 struct Wavelet {
     static constexpr size_t Sigma = TSigma;
 
-    static constexpr auto bits = required_bits(TSigma);
+    static constexpr auto bits = required_bits(TSigma-1);
     static constexpr auto bvct = pow(2, bits);
 
     std::array<Bitvector, bvct> bitvector;
     size_t                      totalLength;
 
+    static auto extractBitsFromSymb(uint8_t symb) -> std::array<std::tuple<uint8_t, uint8_t>, bits> {
+        auto res = std::array<std::tuple<uint8_t, uint8_t>, bits>{};
+
+        for (uint8_t b{0}; b < bits; ++b) {
+            auto bitId          = bits - b - 1;
+            uint8_t bit         = (symb >> bitId) & 1;
+            uint8_t id_offset   = (1ull<<b)-1;
+            uint8_t symb_offset = symb >> (bitId+1);
+            uint8_t id = id_offset + symb_offset;
+            res[b] = {bit, id};
+
+        }
+        return res;
+    }
+    std::array<std::array<std::tuple<uint8_t, uint8_t>, bits>, Sigma> lut{[]() {
+        auto lut = std::array<std::array<std::tuple<uint8_t, uint8_t>, bits>, Sigma>{};
+        for (size_t s{0}; s < Sigma; ++s) {
+            lut[s] = extractBitsFromSymb(s);
+        }
+        return lut;
+    }()};
+
+
     Wavelet() = default;
-    Wavelet(std::span<uint8_t const> _symbols) {
-        totalLength = _symbols.size();
-        auto length = _symbols.size();
-
-        auto which_bv = [](uint64_t symb, auto cb) {
-            uint64_t id{0};
-            uint64_t factor{1};
-            uint64_t mask = 1u << (bits-1);
-            for (uint64_t b{0}; b < bits; ++b) {
-                auto bit = (symb & mask) != 0;
-                cb(id, bit);
-                id += (bit + 1) * factor;
-                factor = factor * 2;
-                mask = mask >> 1;
-            }
-        };
-
-
-        for (uint64_t size{1}; size <= length; ++size) {
-            auto symb = _symbols[size-1];
-            which_bv(symb, [&](uint64_t id, uint64_t bit) {
+    Wavelet(std::span<uint8_t const> _symbols)
+        : totalLength {_symbols.size()} {
+        for (uint64_t size{0}; size < totalLength; ++size) {
+            auto symb = _symbols[size];
+            auto const& res = lut[symb];
+//            auto res = extractBitsFromSymb(symb);
+            for (auto [bit, id] : res) {
                 bitvector[id].push_back(bit);
-            });
+            }
         }
     }
 
@@ -65,81 +75,96 @@ struct Wavelet {
     }
 
     uint8_t symbol(uint64_t idx) const {
-        for (uint64_t i{0}; i < Sigma-1; ++i) {
-            if (rank(idx+1, i) != rank(idx, i)) {
-                return i;
+        uint8_t symb{};
+        for (uint8_t b{0}; b < bits; ++b) {
+            uint8_t id_offset = (1ull<<b)-1;
+            uint8_t id        = id_offset + symb;
+
+            auto [bit, newIdx] = [&]() -> std::tuple<size_t, size_t> {
+                if (id < bitvector.size()) {
+                    size_t bit    = bitvector[id].symbol(idx);
+                    size_t newIdx = bitvector[id].rank(idx);
+                    return {bit, newIdx};
+                } else {
+                    return {0, 0};
+                }
+            }();
+            symb = (symb<<1) | bit;
+            if (bit == 0) {
+                idx = idx - newIdx;
+            } else {
+                idx = newIdx;
             }
+
         }
-        return Sigma-1;
+        return symb;
     }
 
     uint64_t rank(uint64_t idx, uint8_t symb) const {
-        auto which_bv = [](uint64_t symb, auto cb) {
-            uint64_t id{0};
-            uint64_t factor{1};
-            uint64_t mask = 1u << (bits-1);
-            for (uint64_t b{0}; b < bits; ++b) {
-                auto bit = (symb & mask) != 0;
-                cb(id, bit);
-                id += (bit + 1) * factor;
-                factor = factor * 2;
-                mask = mask >> 1;
-            }
-        };
-        uint64_t a = idx;
-        which_bv(symb, [&](uint64_t id, uint64_t value) {
-            auto newIdx = bitvector[id].rank(a);
-            if (value == 0) {
-                a = a - newIdx;
+        auto const& res = lut[symb];
+//        auto res = extractBitsFromSymb(symb);
+        for (auto [bit, id] : res) {
+            size_t newIdx = bitvector[id].rank(idx);
+            if (bit == 0) {
+                idx = idx - newIdx;
             } else {
-                a = newIdx;
+                idx = newIdx;
             }
-        });
-        return a;
+        }
+        return idx;
     }
 
     uint64_t prefix_rank(uint64_t idx, uint8_t symb) const {
-        auto which_bv = [](size_t symb, auto cb) {
-            uint64_t id{0};
-            uint64_t factor{1};
-            uint64_t mask = 1u << (bits-1);
-            for (uint64_t b{0}; b < bits; ++b) {
-                auto bit = (symb & mask) != 0;
-                cb(id, bit);
-                id += (bit + 1) * factor;
-                factor = factor * 2;
-                mask = mask >> 1;
-            }
-        };
         uint64_t a{};
-        uint64_t pos = idx;
-        which_bv(symb+1, [&](uint64_t id, uint8_t value) {
-            auto newIdx = bitvector[id].rank(pos);
-            if (value == 0) {
-                pos = pos - newIdx;
+        auto const& res = lut[symb];
+//        auto res = extractBitsFromSymb(symb);
+        for (auto [bit, id] : res) {
+            size_t newIdx = bitvector[id].rank(idx);
+            if (bit == 0) {
+                idx = idx - newIdx;
             } else {
-                a += pos - newIdx;
-                pos = newIdx;
+                a = a + idx - newIdx;
+                idx = newIdx;
             }
-        });
-
-        return a;
+        }
+        return a + idx;
     }
 
-    auto all_ranks(uint64_t idx) const -> std::array<uint64_t, Sigma> {
-        std::array<uint64_t, Sigma> rs{0};
-        for (uint64_t i{0}; i < Sigma; ++i) {
-            rs[i] = rank(idx, i);
+    template <size_t b=0, size_t symb=0, typename CB>
+    void bv_all(size_t idx, CB const& cb) const {
+        if constexpr(symb >= Sigma) {
+            return;
+        } else if constexpr (b == bits) {
+            cb.template operator()<symb>(idx);
+        } else {
+            uint8_t id_offset = (1ull<<b)-1;
+            uint8_t id        = id_offset + symb;
+
+            auto newIdx = [&]() -> size_t {
+                return bitvector[id].rank(idx);
+            }();
+
+
+            bv_all<b+1, symb<<1>(idx-newIdx, cb);
+            bv_all<b+1, (symb<<1) | 1>(newIdx, cb);
         }
+    }
+
+
+    auto all_ranks(uint64_t idx) const -> std::array<uint64_t, Sigma> {
+        auto rs = std::array<uint64_t, Sigma>{};
+
+        bv_all(idx, [&]<size_t symb>(size_t count) {
+            rs[symb] = count;
+        });
         return rs;
     }
 
     auto all_ranks_and_prefix_ranks(uint64_t idx) const -> std::tuple<std::array<uint64_t, Sigma>, std::array<uint64_t, Sigma>> {
-        std::array<uint64_t, Sigma> rs{0};
-        std::array<uint64_t, Sigma> prs{0};
-        for (uint64_t i{0}; i < Sigma; ++i) {
-            rs[i] = rank(idx, i);
-            prs[i] = prefix_rank(idx, i);
+        auto rs  = all_ranks(idx);
+        auto prs = rs;
+        for (uint64_t i{1}; i < Sigma; ++i) {
+            prs[i] = prs[i-1] + prs[i];
         }
         return {rs, prs};
     }
@@ -148,11 +173,21 @@ struct Wavelet {
     void serialize(Archive& ar) {
         ar(bitvector, totalLength);
     }
-
 };
+
 template <uint64_t TSigma>
 using Wavelet_Default = Wavelet<TSigma>;
-
 static_assert(checkRankVector<Wavelet_Default>);
+
+template <size_t Sigma>
+struct PrunedWavelet : Wavelet<Sigma, bitvector::PrunedBitvector> {
+    using Wavelet<Sigma, bitvector::PrunedBitvector>::Wavelet;
+};
+
+template <uint64_t TSigma>
+using PrunedWavelet_Default = PrunedWavelet<TSigma>;
+
+static_assert(checkRankVector<PrunedWavelet_Default>);
+
 
 }
