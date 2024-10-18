b490e766efdb3385ababdece034a53c976d26a38
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2024-10-18 16:03:15 +0200

    fix: using fmt instead of std::format

--- src/test_fmindex-collection/BenchSize.h
+++ src/test_fmindex-collection/BenchSize.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <format>
+#include <fmt/format.h>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -26,16 +26,16 @@ struct BenchSize {
         auto lines = std::vector<std::string>{};
         lines.resize(entries.size());
 
-        auto addColumn = [&]<typename T>(std::format_string<T&, size_t&> fmt, T BenchSize::Entry::*ptr) {
+        auto addColumn = [&]<typename T>(fmt::format_string<T&, size_t&> fmt, T BenchSize::Entry::*ptr) {
             size_t longestLine{};
             for (size_t i{0}; i < lines.size(); ++i) {
                 size_t x = 1;
-                auto t = std::format(fmt, entries[i].*ptr, x);
+                auto t = fmt::format(fmt, entries[i].*ptr, x);
                 longestLine = std::max(longestLine, t.size());
             }
             for (size_t i{0}; i < lines.size(); ++i) {
-                auto t = std::format(fmt, entries[i].*ptr, longestLine);
-                lines[i] = std::format("{} | {}", lines[i], t);
+                auto t = fmt::format(fmt, entries[i].*ptr, longestLine);
+                lines[i] = fmt::format("{} | {}", lines[i], t);
             }
         };
 
@@ -45,7 +45,7 @@ struct BenchSize {
         addColumn("{:<{}}", &BenchSize::Entry::name);
 
         for (size_t i{0}; i < lines.size(); ++i) {
-            std::cout << std::format("{}\n", lines[i]);
+            std::cout << fmt::format("{}\n", lines[i]);
         }
     }
 };
