f210228ae636a95b3cbe3a1928a9c8a92edd4558
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2024-10-23 11:33:48 +0200

    feat: using cpm.dependencies

--- fmindex_collection-config.cmake
+++ fmindex_collection-config.cmake
@@ -6,13 +6,5 @@ if (TARGET fmindex-collection::fmindex-collection)
     return()
 endif()
 
-set(LIBSAIS_USE_OPENMP ${OpenMP_C_FOUND})
-set(LIBSAIS_BUILD_SHARED_LIB OFF)
-CPMAddPackage("gh:IlyaGrebnov/libsais@2.8.5")
-
-if (FMC_USE_SDSL)
-    CPMAddPackage("gh:SGSSGene/sdsl-lite@3.0.3-2")
-endif()
-
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/src/search_schemes ${CMAKE_CURRENT_BINARY_DIR}/search_schemes)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/src/fmindex-collection ${CMAKE_CURRENT_BINARY_DIR}/fmindex-collection)
