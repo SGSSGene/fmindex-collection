05de1e061b3f3eda62e1e6783488e4b376accb40
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2025-04-03 18:35:16 +0200

    fix: add missing SPDX headers

--- src/test_fmindex-collection/string/benchmark_alphabet_255.cpp
+++ src/test_fmindex-collection/string/benchmark_alphabet_255.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2006-2025, Knut Reinert & Freie Universität Berlin
+// SPDX-FileCopyrightText: 2016-2025, Knut Reinert & MPI für molekulare Genetik
+// SPDX-License-Identifier: CC0-1.0
+
 #include "utils.h"
 
 TEST_CASE("benchmark strings c'tor operation - 255 alphabet", "[string][!benchmark][255][time][ctor][.]") {
