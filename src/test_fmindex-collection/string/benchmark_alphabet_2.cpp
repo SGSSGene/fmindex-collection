03a25944c9c58a717e233021a1fa4c455a261368
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2025-06-12 00:01:33 +0200

    feat: adding a string that uses a single bitvector

---
+++ src/test_fmindex-collection/string/benchmark_alphabet_2.cpp
@@ -0,0 +1,220 @@
+// SPDX-FileCopyrightText: 2006-2025, Knut Reinert & Freie Universität Berlin
+// SPDX-FileCopyrightText: 2016-2025, Knut Reinert & MPI für molekulare Genetik
+// SPDX-License-Identifier: CC0-1.0
+
+#include "utils.h"
+
+namespace fmc = fmindex_collection;
+
+#define MYSTRINGS fmc::string::WrappedBitvector<fmc::bitvector::L0L1_512_64kBitvector>, ALLSTRINGSWITHRANK_IMPL(2)
+
+TEST_CASE("benchmark strings c'tor operation - 2 alphabet", "[string][!benchmark][2][time][ctor]") {
+    auto const& text = generateText<0, 2>();
+
+    SECTION("benchmarking") {
+        auto bench = ankerl::nanobench::Bench{};
+        bench.title("c'tor()")
+             .relative(true)
+             .batch(text.size());
+
+        call_with_templates<MYSTRINGS>([&]<typename Vector>() {
+            if constexpr (std::same_as<Vector, fmc::string::Naive<2>>) {
+                return;
+            }
+
+            auto vector_name = getName<Vector>();
+            INFO(vector_name);
+
+            bench.run(vector_name, [&]() {
+                auto vec = Vector{text};
+                ankerl::nanobench::doNotOptimizeAway(const_cast<Vector const&>(vec));
+            });
+        });
+    }
+}
+
+TEST_CASE("benchmark vectors symbol() operations - 2 alphabet", "[string][!benchmark][2][time][symbol]") {
+    auto const& text = generateText<0, 2>();
+
+    SECTION("benchmarking") {
+        auto bench = ankerl::nanobench::Bench{};
+        bench.title("symbol()")
+             .relative(true)
+             .batch(text.size());
+
+        call_with_templates<
+            MYSTRINGS>([&]<typename Vector>() {
+            if constexpr (std::same_as<Vector, fmc::string::Naive<2>>) {
+                return;
+            }
+
+            auto vector_name = getName<Vector>();
+            INFO(vector_name);
+
+            auto rng = ankerl::nanobench::Rng{};
+
+            auto vec = Vector{text};
+
+            bench.run(vector_name, [&]() {
+                auto v = vec.symbol(rng.bounded(text.size()));
+                ankerl::nanobench::doNotOptimizeAway(v);
+            });
+        });
+    }
+}
+
+TEST_CASE("benchmark vectors rank() operations - 2 alphabet", "[string][!benchmark][2][time][rank]") {
+    auto const& text = generateText<0, 2>();
+
+    SECTION("benchmarking") {
+        auto bench = ankerl::nanobench::Bench{};
+        bench.title("rank()")
+             .relative(true);
+
+        call_with_templates<
+            MYSTRINGS>([&]<typename Vector>() {
+            if constexpr (std::same_as<Vector, fmc::string::Naive<2>>) {
+                return;
+            }
+
+            auto vector_name = getName<Vector>();
+            INFO(vector_name);
+
+            auto rng = ankerl::nanobench::Rng{};
+
+            auto vec = Vector{text};
+
+            bench.run(vector_name, [&]() {
+                auto v = vec.rank(rng.bounded(text.size()+1), rng.bounded(2));
+                ankerl::nanobench::doNotOptimizeAway(v);
+            });
+        });
+    }
+}
+
+TEST_CASE("benchmark vectors prefix_rank() operations - 2 alphabet", "[string][!benchmark][2][time][prefix_rank]") {
+    auto const& text = generateText<0, 2>();
+
+    SECTION("benchmarking") {
+        auto bench = ankerl::nanobench::Bench{};
+        bench.title("prefix_rank()")
+             .relative(true);
+
+        call_with_templates<
+            MYSTRINGS>([&]<typename Vector>() {
+            if constexpr (std::same_as<Vector, fmc::string::Naive<2>>) {
+                return;
+            }
+
+            auto vector_name = getName<Vector>();
+            INFO(vector_name);
+
+            auto rng = ankerl::nanobench::Rng{};
+
+            auto vec = Vector{text};
+
+            bench.run(vector_name, [&]() {
+                auto v = vec.prefix_rank(rng.bounded(text.size()+1), rng.bounded(2));
+                ankerl::nanobench::doNotOptimizeAway(v);
+            });
+        });
+    }
+}
+
+TEST_CASE("benchmark vectors all_ranks() operations - 2 alphabet", "[string][!benchmark][2][time][all_ranks]") {
+    auto const& text = generateText<0, 2>();
+
+    SECTION("benchmarking") {
+        auto bench = ankerl::nanobench::Bench{};
+        bench.title("all_ranks()")
+             .relative(true);
+
+        call_with_templates<
+            MYSTRINGS>([&]<typename Vector>() {
+            if constexpr (std::same_as<Vector, fmc::string::Naive<2>>) {
+                return;
+            }
+
+            auto vector_name = getName<Vector>();
+            INFO(vector_name);
+
+            auto rng = ankerl::nanobench::Rng{};
+
+            auto vec = Vector{text};
+
+            bench.run(vector_name, [&]() {
+                auto v = vec.all_ranks(rng.bounded(text.size()+1));
+                ankerl::nanobench::doNotOptimizeAway(v);
+            });
+        });
+    }
+}
+
+TEST_CASE("benchmark vectors all_ranks_and_prefix_ranks() operations - 2 alphabet", "[string][!benchmark][2][time][all_ranks_and_prefix_ranks]") {
+    auto const& text = generateText<0, 2>();
+
+    SECTION("benchmarking") {
+        auto bench = ankerl::nanobench::Bench{};
+        bench.title("all_ranks_and_prefix_ranks()")
+             .relative(true);
+
+        call_with_templates<
+            MYSTRINGS>([&]<typename Vector>() {
+            if constexpr (std::same_as<Vector, fmc::string::Naive<2>>) {
+                return;
+            }
+
+            auto vector_name = getName<Vector>();
+            INFO(vector_name);
+
+            auto rng = ankerl::nanobench::Rng{};
+
+            auto vec = Vector{text};
+
+            bench.run(vector_name, [&]() {
+                auto v = vec.all_ranks_and_prefix_ranks(rng.bounded(text.size()+1));
+                ankerl::nanobench::doNotOptimizeAway(v);
+            });
+        });
+    }
+}
+
+TEST_CASE("benchmark vectors in size - alphabet 2", "[string][!benchmark][2][size]") {
+    auto const& text = generateText<0, 2>();
+
+    SECTION("benchmarking") {
+        BenchSize benchSize;
+        benchSize.baseSize = 2.;
+        benchSize.entries[0][2] = "bits/char";
+        benchSize.entries[0][2] = "alphabet 2";
+
+        call_with_templates<MYSTRINGS>([&]<typename Vector>() {
+            if constexpr (std::same_as<Vector, fmc::string::Naive<2>>) {
+                return;
+            }
+
+            auto vector_name = getName<Vector>();
+            INFO(vector_name);
+
+            auto rng = ankerl::nanobench::Rng{};
+
+            auto vec = Vector{text};
+            auto size = [&]() {
+                if constexpr (requires() { vec.space_usage(); }) {
+                    return vec.space_usage();
+                } else {
+                    auto ofs     = std::stringstream{};
+                    auto archive = cereal::BinaryOutputArchive{ofs};
+                    archive(vec);
+                    return ofs.str().size();
+                }
+            }();
+            benchSize.addEntry({
+                .name = vector_name,
+                .size = size,
+                .text_size = text.size(),
+                .bits_per_char = (size*8)/double(text.size())
+            });
+        });
+    }
+}
