3034c32baf8e171bcf84cd090d6cdea116af6b35
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2025-09-13 10:13:11 +0200

    fix: AwFMIndex now uses FlattenedBitvctors_L0L1

--- src/test_fmindex-collection/string/AWFMIndex.h
+++ src/test_fmindex-collection/string/AWFMIndex.h
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 #pragma once
 
-#include <fmindex-collection/string/L0L1_NEPRV9.h>
+#include <fmindex-collection/string/FlattenedBitvectors_L0L1.h>
 
 extern "C" {
 #include "AwFmIndex.h"
@@ -97,10 +97,10 @@ inline void awFmIndexCreateStringWithRankSupport(struct AwFmIndex* _RESTRICT_ co
     }
 }
 
-// If AWFMIndex doesn't implement the correct version, fall back to L0L1NEPRV9 version, which has similar memory usage
+// If AWFMIndex doesn't implement the correct version, fall back to FlattenedBitvectors_L0L1 version, which has similar memory usage
 template <size_t TSigma>
-struct AWFMIndex : fmc::string::L0L1_NEPRV9<TSigma, 64, 65536> {
-    using fmc::string::L0L1_NEPRV9<TSigma, 64, 65536>::L0L1_NEPRV9;
+struct AWFMIndex : fmc::string::FlattenedBitvectors_L0L1<TSigma, 64, 65536> {
+    using fmc::string::FlattenedBitvectors_L0L1<TSigma, 64, 65536>::FlattenedBitvectors_L0L1;
 };
 /**
  * This is a quick and dirty implementation
