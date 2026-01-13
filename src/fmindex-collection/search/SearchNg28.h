584e152ce834fb6f6cd590c8f24c7d098b62d962
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2026-01-13 18:51:59 +0100

    patch: removed all template parameters from SearchNG28

--- src/fmindex-collection/search/SearchNg28.h
+++ src/fmindex-collection/search/SearchNg28.h
@@ -33,14 +33,22 @@ struct Search {
     mutable std::vector<size_t> partition;
     delegate_t const& delegate;
 
+    enum dir_t : int {
+        Left = -1,
+        Right = 1,
+    };
+
     struct Side {
         uint8_t lastRank{};
         uint8_t lastQRank{};
+        char    info{'M'};
+        size_t  queryPos{};
     };
-    mutable std::array<Side, 2> side;
+    mutable std::array<Side, 3> sideImpl;
+    mutable Side* side{nullptr};
     mutable size_t e{};
     mutable size_t part{};
-    mutable size_t queryPosL{}, queryPosR{};
+    mutable dir_t dir{dir_t::Right};
 
     Search(index_t const& _index, query_t const& _query, search_t const& _search, std::vector<size_t> const& _partition, delegate_t const& _delegate)
         : index     {_index}
@@ -51,32 +59,30 @@ struct Search {
     {
         // check how many characters are before the first query char
         for (size_t i{0}; i < search.pi[0]; ++i) {
-            queryPosL += partition[i];
-            queryPosR += partition[i];
+            sideImpl[0].queryPos += partition[i];
+            sideImpl[2].queryPos += partition[i];
         }
-        // Notice: queryPosL might 'underflow' (also at other spots)
+        // Notice: sideImpl[0].queryPos might 'underflow' (also at other spots)
         // but that is fine, after an underflow this variable is not
         // being accessed before overflowing again.
-        queryPosL -= 1;
+        sideImpl[0].queryPos -= 1;
     }
 
     bool run() {
-        auto cur       = cursor_t{index};
-        return search_next<'M', 'M'>(cur);
+        auto cur = cursor_t{index};
+        return search_next_part(cur);
     }
 
 
-    template <bool Right>
-    static auto extend(cursor_t const& cur, uint64_t symb) noexcept {
-        if constexpr (Right) {
+    auto extend(cursor_t const& cur, uint64_t symb) const noexcept {
+        if (dir == dir_t::Right) {
             return cur.extendRight(symb);
         } else {
             return cur.extendLeft(symb);
         }
     }
-    template <bool Right>
-    static auto extend(cursor_t const& cur) noexcept {
-        if constexpr (Right) {
+    auto extend(cursor_t const& cur) const noexcept {
+        if (dir == dir_t::Right) {
             return cur.extendRight();
         } else {
             return cur.extendLeft();
@@ -84,13 +90,14 @@ struct Search {
     }
 
 
-    template <char LInfo, char RInfo>
-    bool search_next(cursor_t const& cur) const {
+    bool search_next_part(cursor_t const& cur) const {
         if (cur.count() == 0) {
             return false;
         }
         if (part == partition.size()) {
-            if constexpr (!Edit || ((LInfo == 'M' or LInfo == 'I') and (RInfo == 'M' or RInfo == 'I'))) {
+            auto il = sideImpl[0].info;
+            auto ir = sideImpl[2].info;
+            if (!Edit || ((il == 'M' or il == 'I') and (ir == 'M' or ir == 'I'))) {
                 if (search.l.back() <= e and e <= search.u.back()) {
                     return delegate(cur, e);
                 }
@@ -98,168 +105,124 @@ struct Search {
             return false;
         }
 
-        bool Right = (part==0) || (search.pi[part-1] < search.pi[part]);
-        if (cur.count() > 1) {
-            if (Right) {
-                return search_next_dir<LInfo, RInfo, true>(cur);
-            } else {
-                return search_next_dir<LInfo, RInfo, false>(cur);
-            }
-        } else {
-            if (Right) {
-                return search_next_dir_single<LInfo, RInfo, true>(cur);
-            } else {
-                return search_next_dir_single<LInfo, RInfo, false>(cur);
-            }
-        }
+        bool right = (part==0) || (search.pi[part-1] < search.pi[part]);
+        auto r_dir = Restore{dir, right?dir_t::Right:dir_t::Left};
+        auto r_side = Restore{side, right?&sideImpl[2]:&sideImpl[0]};
+        return search_next_symb(cur);
     }
 
-    template <char LInfo, char RInfo, bool Right, bool NextPos>
-    bool search_next_pos(cursor_t const& cur) const {
+    bool check_and_search_next_symb(cursor_t const& cur) const {
         if (cur.count() == 0) return false;
-        if constexpr (NextPos) {
-            if constexpr (Right) queryPosR += 1;
-            else queryPosL -= 1;
-            partition[search.pi[part]] -= 1;
+        auto r_qp = RestoreAdd{side->queryPos, dir};
+        auto r_p  = RestoreSub{partition[search.pi[part]], 1};
 
-            if (partition[search.pi[part]] == 0) {
-                part += 1;
-                bool res = search_next<LInfo, RInfo>(cur);
-                part -= 1;
-                partition[search.pi[part]] += 1;
-                if constexpr (Right) queryPosR -= 1;
-                else queryPosL += 1;
-                return res;
-            }
+        if (partition[search.pi[part]] == 0) {
+            auto r_p = RestoreAdd{part, 1};
+            return search_next_part(cur);
         }
-
-        bool r = [&]() {
-            if (cur.count() > 1) {
-                return search_next_dir<LInfo, RInfo, Right>(cur);
-            } else {
-                return search_next_dir_single<LInfo, RInfo, Right>(cur);
-            }
-        }();
-        if constexpr (NextPos) {
-            if constexpr (Right) queryPosR -= 1;
-            else queryPosL += 1;
-            partition[search.pi[part]] += 1;
-        }
-        return r;
+        return search_next_symb(cur);
     }
 
-    template <char LInfo, char RInfo, bool Right>
-    bool search_next_dir(cursor_t const& cur) const {
-        static constexpr char TInfo = Right ? RInfo : LInfo;
 
-        constexpr bool Deletion     = (TInfo != 'S' && TInfo != 'I') && Edit;
-        constexpr bool Insertion    = (TInfo != 'S' && TInfo != 'D') && Edit;
+    auto computeErrorCases() const {
+        auto const tinfo               = side->info;
+        bool const mismatchAllowed     = e+1 <= search.u[part];
+        bool const substitutionAllowed = search.l[part] <= e+partition[search.pi[part]] and e+1 <= search.u[part];
+        bool const insertionAllowed    = Edit && tinfo != 'S' && tinfo != 'D' && substitutionAllowed;
+        bool const deletionAllowed     = Edit && tinfo != 'S' && tinfo != 'I' && mismatchAllowed;
+        return std::make_tuple(mismatchAllowed, substitutionAllowed, insertionAllowed, deletionAllowed);
+    }
+    auto computeMatchCase(size_t nextSymb) const {
+        auto const tinfo        = side->info;
+        bool const matchAllowed = (partition[search.pi[part]] > 1 or search.l[part] <= e)
+                                 and e <= search.u[part]
+                                 and (tinfo != 'I' or nextSymb != side->lastQRank)
+                                 and (tinfo != 'D' or nextSymb != side->lastRank);
+        return matchAllowed;
+    }
 
-        constexpr char OnMatchL      = Right ? LInfo : 'M';
-        constexpr char OnMatchR      = Right ? 'M'   : RInfo;
-        constexpr char OnSubstituteL = Right ? LInfo : 'S';
-        constexpr char OnSubstituteR = Right ? 'S'   : RInfo;
-        constexpr char OnDeletionL   = Right ? LInfo : 'D';
-        constexpr char OnDeletionR   = Right ? 'D'   : RInfo;
-        constexpr char OnInsertionL  = Right ? LInfo : 'I';
-        constexpr char OnInsertionR  = Right ? 'I'   : RInfo;
+    bool search_next_symb(cursor_t const& cur) const {
+        if (cur.count() == 1) {
+            return search_next_symb_single(cur);
+        }
 
-        auto nextSymb = query[Right?queryPosR:queryPosL];
+        auto nextSymb = query[side->queryPos];
 
-        bool matchAllowed    = (partition[search.pi[part]] > 1 or search.l[part] <= e)
-                               and e <= search.u[part]
-                               and (TInfo != 'I' or nextSymb != side[Right].lastQRank)
-                               and (TInfo != 'D' or nextSymb != side[Right].lastRank);
-        bool insertionAllowed    = (partition[search.pi[part]] > 1 or search.l[part] <= e+1)
-                                   and e+1 <= search.u[part];
-        bool substitutionAllowed = insertionAllowed;
-        bool mismatchAllowed     = e+1 <= search.u[part];
+        auto const matchAllowed = computeMatchCase(nextSymb);
+        auto [mismatchAllowed, substitutionAllowed, insertionAllowed, deletionAllowed] = computeErrorCases();
 
         if (mismatchAllowed) {
-            auto cursors = extend<Right>(cur);
+            auto r_i   = Restore{side->info, 'M'};
+            auto cursors = extend(cur);
 
             if (matchAllowed) {
                 auto const& newCur = cursors[nextSymb];
-                auto s1 = Restore{side[Right].lastRank, nextSymb};
-                auto s2 = Restore{side[Right].lastQRank, nextSymb};
-                auto f = search_next_pos<OnMatchL, OnMatchR, Right, /*.nextPos=*/true>(newCur);
+                auto r_lr  = Restore{side->lastRank, nextSymb};
+                auto r_lqr = Restore{side->lastQRank, nextSymb};
+                side->info = 'M';
+                auto f = check_and_search_next_symb(newCur);
                 if (f) return true;
             }
 
-            auto se = Restore{e, e+1};
+            auto r_e = Restore{e, e+1};
             for (uint64_t i{FirstSymb}; i < Sigma; ++i) {
                 auto const& newCur = cursors[i];
 
-                auto s1 = Restore{side[Right].lastRank, i};
-                if constexpr (Deletion) {
-                    auto f = search_next_pos<OnDeletionL, OnDeletionR, Right, /*.nextPos=*/false>(newCur); // deletion occurred in query
+                auto r_lr = Restore{side->lastRank, i};
+                if (deletionAllowed) {
+                    side->info = 'D';
+                    auto f = search_next_symb(newCur); // deletion occurred in query
                     if (f) return true;
                 }
                 if (!substitutionAllowed) continue;
                 if (i == nextSymb) continue;
 
-                auto s2 = Restore{side[Right].lastQRank, nextSymb};
-                auto f = search_next_pos<OnSubstituteL, OnSubstituteR, Right, /*.nextPos=*/true>(newCur);
+                auto r_lqr = Restore{side->lastQRank, nextSymb};
+                side->info = 'S';
+                auto f = check_and_search_next_symb(newCur);
                 if (f) return true;
             }
 
-            if constexpr (Insertion) {
-                if (insertionAllowed) {
-                    auto s2 = Restore{side[Right].lastQRank, nextSymb};
-                    auto f = search_next_pos<OnInsertionL, OnInsertionR, Right, /*.nextPos=*/true>(cur); // insertion occurred in query
-                    if (f) return true;
-                }
+            if (insertionAllowed) {
+                auto r_lqr = Restore{side->lastQRank, nextSymb};
+                side->info = 'I';
+                auto f = check_and_search_next_symb(cur); // insertion occurred in query
+                if (f) return true;
             }
         } else if (matchAllowed) {
-            auto f = search_next_dir_no_errors<OnMatchL, OnMatchR, Right>(cur);
-            if (f) return true;
+            return search_next_part_no_errors(cur);
         }
         return false;
     }
 
-    template <char LInfo, char RInfo, bool Right>
-    bool search_next_dir_no_errors(cursor_t cur) const {
+    /** Searches the (rest of) a part without any errors
+     *
+     * May only be called if no errors are allowed
+     */
+    bool search_next_part_no_errors(cursor_t cur) const {
+        assert(e == search.u[part]);
+
         auto loops = partition[search.pi[part]];
         auto nextSymb = decltype(query[0]){};
         for (size_t i{0}; i < loops; ++i) {
-            nextSymb = query[Right?(queryPosR+i):(queryPosL-i)];
-            cur = extend<Right>(cur, nextSymb);
-            if (cur.count() == 0) return false;
+            auto pos = side->queryPos + i*dir;
+            nextSymb = query[pos];
+            cur = extend(cur, nextSymb);
+            if (cur.count() == 0) return false; // early abort, if results are empty
         }
 
-        auto s1 = Restore{side[Right].lastRank, nextSymb};
-        auto s2 = Restore{side[Right].lastQRank, nextSymb};
-        partition[search.pi[part]] = 0;
-        part += 1;
-        if constexpr (Right) queryPosR += loops;
-        else queryPosL -= loops;
-        bool res = search_next<LInfo, RInfo>(cur);
-        part -= 1;
-        partition[search.pi[part]] = loops;
-        if constexpr (Right) queryPosR -= loops;
-        else queryPosL += loops;
-        return res;
+        auto r_lr   = Restore{side->lastRank, nextSymb};
+        auto r_lqr  = Restore{side->lastQRank, nextSymb};
+        auto r_p    = Restore{partition[search.pi[part]], 0};
+        auto r_part = Restore{part, part+1};
+        auto r_qp   = RestoreAdd{side->queryPos, loops * dir};
+        auto r_i    = Restore{side->info, 'M'};
+        return search_next_part(cur);
     }
 
-    template <char LInfo, char RInfo, bool Right>
-    bool search_next_dir_single(cursor_t const& cur) const {
-        static constexpr char TInfo = Right ? RInfo : LInfo;
-
-        constexpr bool Deletion     = (TInfo != 'S' && TInfo != 'I') && Edit;
-        constexpr bool Insertion    = (TInfo != 'S' && TInfo != 'D') && Edit;
-
-        constexpr char OnMatchL      = Right ? LInfo : 'M';
-        constexpr char OnMatchR      = Right ? 'M'   : RInfo;
-        constexpr char OnSubstituteL = Right ? LInfo : 'S';
-        constexpr char OnSubstituteR = Right ? 'S'   : RInfo;
-        constexpr char OnDeletionL   = Right ? LInfo : 'D';
-        constexpr char OnDeletionR   = Right ? 'D'   : RInfo;
-        constexpr char OnInsertionL  = Right ? LInfo : 'I';
-        constexpr char OnInsertionR  = Right ? 'I'   : RInfo;
-
-
+    bool search_next_symb_single(cursor_t const& cur) const {
         auto [curISymb, icursorNext] = [&]() -> std::tuple<size_t, cursor_t> {
-            if constexpr (Right) {
+            if (dir == dir_t::Right) {
                 auto symb = cur.symbolRight();
                 auto cur_  = cur.extendRight(symb);
                 return {symb, cur_};
@@ -270,20 +233,16 @@ struct Search {
             }
         }();
 
-        auto curQSymb = query[Right?queryPosR:queryPosL];
+        auto curQSymb = query[side->queryPos];
 
-        bool insertionAllowed    = search.l[part] <= e+partition[search.pi[part]] and e+1 <= search.u[part];
-        bool substitutionAllowed = insertionAllowed;
-        bool mismatchAllowed     = e+1 <= search.u[part];
+        auto [mismatchAllowed, substitutionAllowed, insertionAllowed, deletionAllowed] = computeErrorCases();
 
-        if constexpr (Insertion) {
-            if (insertionAllowed) {
-                auto se = Restore{e, e+1};
-                auto s2 = Restore{side[Right].lastQRank, curQSymb};
-
-                bool f = search_next_pos<OnInsertionL, OnInsertionR, Right, /*.nextPos=*/true>(cur);
-                if (f) return true;
-            }
+        if (insertionAllowed) {
+            auto r_e   = Restore{e, e+1};
+            auto r_lqr = Restore{side->lastQRank, curQSymb};
+            auto r_i   = Restore{side->info, 'I'};
+            bool f = check_and_search_next_symb(cur);
+            if (f) return true;
         }
 
         // only insertions are possible
@@ -291,49 +250,37 @@ struct Search {
             return false;
         }
 
-        bool matchAllowed    = (partition[search.pi[part]] > 1 or search.l[part] <= e)
-                               and e <= search.u[part]
-                               and (TInfo != 'I' or curQSymb != side[Right].lastQRank)
-                               and (TInfo != 'D' or curQSymb != side[Right].lastRank);
+        auto const matchAllowed = computeMatchCase(curQSymb);
 
+        auto r_i = Restore{side->info};
         if (curISymb == curQSymb) {
             if (matchAllowed) {
                 if (!mismatchAllowed) {
-                    auto f = search_next_dir_no_errors<OnMatchL, OnMatchR, Right>(cur);
-                    if (f) return true;
-                    return false;
+                    return search_next_part_no_errors(cur);
                 }
-                auto s1 = Restore{side[Right].lastRank, curQSymb};
-                auto s2 = Restore{side[Right].lastQRank, curQSymb};
-                bool f = search_next_pos<OnMatchL, OnMatchR, Right, /*.nextPos=*/true>(icursorNext);
+                auto r_lr  = Restore{side->lastRank, curQSymb};
+                auto r_lqr = Restore{side->lastQRank, curQSymb};
+                side->info = 'M';
+                bool f = check_and_search_next_symb(icursorNext);
                 if (f) return true;
             }
-            if constexpr (Deletion) {
-                if (mismatchAllowed) {
-                    auto se = Restore{e, e+1};
-                    auto s1 = Restore{side[Right].lastRank, curISymb};
-                    bool f = search_next_pos<OnDeletionL, OnDeletionR, Right, /*.nextPos=*/false>(icursorNext);
-                    if (f) return true;
-                }
-            }
-        } else if (mismatchAllowed) {
-            auto se = Restore{e, e+1};
-
+        } else if (substitutionAllowed) {
             // search substitute
-            if (substitutionAllowed) {
-                auto s1 = Restore{side[Right].lastRank, curISymb};
-                auto s2 = Restore{side[Right].lastQRank, curQSymb};
-
-                bool f = search_next_pos<OnSubstituteL, OnSubstituteR, Right, /*.nextPos=*/true>(icursorNext);
-                if (f) return true;
-            }
-
-            if constexpr (Deletion) {
-                auto s1 = Restore{side[Right].lastRank, curISymb};
-                bool f = search_next_pos<OnDeletionL, OnDeletionR, Right, /*.nextPos=*/false>(icursorNext);
-                if (f) return true;
-            }
+            auto r_e = Restore{e, e+1};
+            auto r_lr  = Restore{side->lastRank, curISymb};
+            auto r_lqr = Restore{side->lastQRank, curQSymb};
+            side->info = 'S';
+            bool f = check_and_search_next_symb(icursorNext);
+            if (f) return true;
         }
+        if (deletionAllowed) {
+            auto r_e  = Restore{e, e+1};
+            auto r_lr = Restore{side->lastRank, curISymb};
+            side->info = 'D';
+            bool f = search_next_symb(icursorNext);
+            if (f) return true;
+        }
+
         return false;
     }
 };
